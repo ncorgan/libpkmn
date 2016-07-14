4427f0a63a7e92bb46cc2067941bdba0eb216cc2
Nicholas Corgan <n.corgan@gmail.com>
2016-07-14 03:30:37 -0500

    Removed references to PKSav

--- include/pkmn/config.hpp
+++ include/pkmn/config.hpp
@@ -16,38 +16,38 @@
  * Define cross-platform macros
  ***************************************************************************/
 #if defined(_MSC_VER)
-#    define PKSAV_EXPORT         __declspec(dllexport)
-#    define PKSAV_IMPORT         __declspec(dllimport)
-#    define PKSAV_INLINE         __forceinline
-#    define PKSAV_UNUSED(x)      x
+#    define PKMN_EXPORT         __declspec(dllexport)
+#    define PKMN_IMPORT         __declspec(dllimport)
+#    define PKMN_INLINE         __forceinline
+#    define PKMN_UNUSED(x)      x
 #    pragma warning(disable: 4251) // class 'A<T>' needs to have dll-interface to be used by clients of class 'B'
 #    pragma warning(disable: 4800) // forcing value to bool 'true' or 'false'
 #elif defined(__GNUG__) && __GNUG__ >= 4
-#    define PKSAV_EXPORT         __attribute__((visibility("default")))
-#    define PKSAV_IMPORT         __attribute__((visibility("default")))
-#    define PKSAV_INLINE         inline __attribute__((always_inline))
-#    define PKSAV_UNUSED(x)      x __attribute__((unused))
+#    define PKMN_EXPORT         __attribute__((visibility("default")))
+#    define PKMN_IMPORT         __attribute__((visibility("default")))
+#    define PKMN_INLINE         inline __attribute__((always_inline))
+#    define PKMN_UNUSED(x)      x __attribute__((unused))
 #else
-#    define PKSAV_EXPORT
-#    define PKSAV_IMPORT
-#    define PKSAV_INLINE         inline
-#    define PKSAV_UNUSED(x)      x
+#    define PKMN_EXPORT
+#    define PKMN_IMPORT
+#    define PKMN_INLINE         inline
+#    define PKMN_UNUSED(x)      x
 #endif
 
-#ifdef PKSAV_DLL_EXPORTS
-#    define PKSAV_API PKSAV_EXPORT
+#ifdef PKMN_DLL_EXPORTS
+#    define PKMN_API PKMN_EXPORT
 #else
-#    define PKSAV_API PKSAV_IMPORT
+#    define PKMN_API PKMN_IMPORT
 #endif
 
 #if defined(linux) || defined(__linux) || defined(__linux__)
-#    define PKSAV_PLATFORM_LINUX
+#    define PKMN_PLATFORM_LINUX
 #elif defined(__MINGW32__) || defined(MINGW32)
-#    define PKSAV_PLATFORM_MINGW
+#    define PKMN_PLATFORM_MINGW
 #elif defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
-#    define PKSAV_PLATFORM_WIN32
+#    define PKMN_PLATFORM_WIN32
 #elif defined(macintosh) || defined(__APPLE__) || defined(__APPLE_CC__)
-#    define PKSAV_PLATFORM_MACOS
+#    define PKMN_PLATFORM_MACOS
 #endif
 
 #endif /* PKMN_CONFIG_HPP */
