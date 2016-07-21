642f94fc5c44e3dbfe30595e05bbcb12d8ccf5d3
Nicholas Corgan <n.corgan@gmail.com>
2016-07-21 08:25:31 -0500

    calculations: documented forms-related functions

--- include/pkmn/calculations/form.hpp
+++ include/pkmn/calculations/form.hpp
@@ -14,6 +14,16 @@
 
 namespace pkmn { namespace calculations {
 
+    /*!
+     * @brief Returns a Generation II Unown's form based on IV's.
+     *
+     * \param IV_attack Attack IV (0-15)
+     * \param IV_defense Defense IV (0-15)
+     * \param IV_speed Speed IV (0-15)
+     * \param IV_special Special IV (0-15)
+     * \throws std::out_of_range if any IV is outside the range [0,15]
+     * \returns Unown form, possible values: "A"-"Z"
+     */
     PKMN_API std::string gen2_unown_form(
         int IV_attack,
         int IV_defense,
@@ -21,10 +31,24 @@ namespace pkmn { namespace calculations {
         int IV_special
     );
 
+    /*!
+     * @brief Returns a Generation III Unown's form based on its personality.
+     *
+     * \param personality Unown's personality value
+     * \returns Unown form, possible values: "A"-"Z","?","!"
+     */
     PKMN_API std::string gen3_unown_form(
         uint32_t personality
     );
 
+    /*!
+     * @brief Returns whether or not a Wurmple with a given personality will
+     *        evolve into a Silcoon.
+     *
+     * \param personality Wurmple's personality
+     * \param before_gen5 Whether the Wurmple is in a Generation III-IV game
+     * \returns Whether a Wurmple will evolve into a Silcoon
+     */
     PKMN_INLINE bool wurmple_becomes_silcoon(
         uint32_t personality,
         bool before_gen5
--- lib/calculations/form.cpp
+++ lib/calculations/form.cpp
@@ -7,6 +7,8 @@
 
 #include <pkmn/calculations/form.hpp>
 
+#include <stdexcept>
+
 namespace pkmn { namespace calculations {
 
     std::string gen2_unown_form(
@@ -15,6 +17,19 @@ namespace pkmn { namespace calculations {
         int IV_speed,
         int IV_special
     ) {
+        if(IV_attack < 0 or IV_attack > 15) {
+            throw std::out_of_range("IV_attack: valid range 0-15");
+        }
+        if(IV_defense < 0 or IV_defense > 15) {
+            throw std::out_of_range("IV_defense: valid range 0-15");
+        }
+        if(IV_speed < 0 or IV_speed > 15) {
+            throw std::out_of_range("IV_speed: valid range 0-15");
+        }
+        if(IV_special < 0 or IV_special > 15) {
+            throw std::out_of_range("IV_special: valid range 0-15");
+        }
+
         uint8_t alphabet_num = ((uint8_t(IV_attack) << 5)
                              | (uint8_t(IV_defense) << 3)
                              | (uint8_t(IV_speed) << 1)
