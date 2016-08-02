c187445499adb1c62760b90e446f273918f67f13
Nicholas Corgan <n.corgan@gmail.com>
2016-08-02 05:35:41 -0600

    tests: added skeleton database entry test

---
+++ tests/cpp_database_entry_test.cpp
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2016 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+
+#include <pkmn/database/item_entry.hpp>
+#include <pkmn/database/move_entry.hpp>
+#include <pkmn/database/pokemon_entry.hpp>
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(item_entry_test) {
+    (void)0;
+}
+
+BOOST_AUTO_TEST_CASE(move_entry_test) {
+    (void)0;
+}
+
+BOOST_AUTO_TEST_CASE(pokemon_entry_test) {
+    (void)0;
+}
