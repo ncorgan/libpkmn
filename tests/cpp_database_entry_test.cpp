9dcc9032a6b601702278dfedd16a209623e0f2e0
Nicholas Corgan <n.corgan@gmail.com>
2016-08-02 05:49:13 -0600

    tests: starting item entry test

--- tests/cpp_database_entry_test.cpp
+++ tests/cpp_database_entry_test.cpp
@@ -11,8 +11,41 @@
 
 #include <boost/test/unit_test.hpp>
 
+static void item_entry_test_common(
+    const pkmn::database::item_entry &item_entry_gen1,
+    const pkmn::database::item_entry &item_entry_gen2,
+    const pkmn::database::item_entry &item_entry_gba,
+    const pkmn::database::item_entry &item_entry_gcn,
+    const pkmn::database::item_entry &item_entry_gen4,
+    const pkmn::database::item_entry &item_entry_gen5,
+    const pkmn::database::item_entry &item_entry_gen6
+) {
+    (void)item_entry_gen1;
+    (void)item_entry_gen2;
+    (void)item_entry_gba;
+    (void)item_entry_gcn;
+    (void)item_entry_gen4;
+    (void)item_entry_gen5;
+    (void)item_entry_gen6;
+}
+
 BOOST_AUTO_TEST_CASE(item_entry_test) {
-    (void)0;
+    // TODO: get equivalent entries created by index
+
+    // Test with item entries created by name
+    pkmn::database::item_entry byname_gen1("Potion", "Red");
+    pkmn::database::item_entry byname_gen2("Pink Bow", "Silver");
+    pkmn::database::item_entry byname_gba("Razz Berry", "Emerald");
+    pkmn::database::item_entry byname_gcn("Battle CD 03", "XD");
+    pkmn::database::item_entry byname_gen4("Fast Ball", "SoulSilver");
+    pkmn::database::item_entry byname_gen5("TM05", "Black");
+    pkmn::database::item_entry byname_gen6("Salamencite", "Omega Ruby");
+
+    item_entry_test_common(
+        byname_gen1, byname_gen2, byname_gba,
+        byname_gcn, byname_gen4, byname_gen5,
+        byname_gen6
+    );
 }
 
 BOOST_AUTO_TEST_CASE(move_entry_test) {
