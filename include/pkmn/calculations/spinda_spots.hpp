e93ab455ce775a7a7d0c2636f1cc93bcaf519cf1
Nicholas Corgan <n.corgan@gmail.com>
2016-08-08 07:32:19 -0500

    Added documentation,operators,constructors for Spinda-related structs

--- include/pkmn/calculations/spinda_spots.hpp
+++ include/pkmn/calculations/spinda_spots.hpp
@@ -13,23 +13,42 @@
 
 namespace pkmn { namespace calculations {
 
+    //! X/Y coordinates of an indiviudal Spinda spot.
     struct spinda_coords {
+        //! Default constructor.
         PKMN_CONSTEXPR_OR_INLINE spinda_coords():
             x(0), y(0) {}
 
+        /*!
+         * @brief Constructor taking X and Y coordinates.
+         * \param xcoord X coordinate
+         * \param ycoord Y coordinate
+         */
         PKMN_CONSTEXPR_OR_INLINE spinda_coords(
             int xcoord, int ycoord
         ): x(xcoord), y(ycoord) {}
 
+        /*!
+         * @brief Copy constructor.
+         * \param other Coordinates to copy
+         */
         PKMN_CONSTEXPR_OR_INLINE spinda_coords(
             const spinda_coords &other
         ): x(other.x), y(other.y) {}
 
 #ifndef SWIG
+        /*!
+         * @brief Move constructor.
+         * \param other Reference to coordinates whose members to move
+         */
         PKMN_CONSTEXPR_OR_INLINE spinda_coords(
             spinda_coords&& other
         ): x(other.x), y(other.y) {}
 
+        /*!
+         * @brief Assignment operator.
+         * \param rhs Coordinates whose members to assign.
+         */
         PKMN_INLINE spinda_coords& operator=(
             const spinda_coords &rhs
         ) {
@@ -39,11 +58,18 @@ namespace pkmn { namespace calculations {
         }
 #endif
 
+        //! X coordinate.
         int x;
+        //! Y coordinate.
         int y;
     };
 
 #ifndef SWIG
+    /*!
+     * @brief Equality check between two Spinda spot coordinates.
+     * \param lhs Coordinates to left of == operator
+     * \param rhs Coordinates to right of == operator
+     */
     PKMN_CONSTEXPR_OR_INLINE bool operator==(
         const spinda_coords &lhs,
         const spinda_coords &rhs
@@ -51,6 +77,11 @@ namespace pkmn { namespace calculations {
         return (lhs.x == rhs.x) and (lhs.y == rhs.y);
     }
 
+    /*!
+     * @brief Inequality check between two Spinda spot coordinates.
+     * \param lhs Coordinates to left of != operator
+     * \param rhs Coordinates to right of != operator
+     */
     PKMN_CONSTEXPR_OR_INLINE bool operator!=(
         const spinda_coords &lhs,
         const spinda_coords &rhs
@@ -59,13 +90,22 @@ namespace pkmn { namespace calculations {
     }
 #endif
 
+    //! Coordinates of all four Spinda spots.
     struct spinda_spots {
+        //! Default constructor.
         PKMN_CONSTEXPR_OR_INLINE spinda_spots():
             top_left(spinda_coords()),
             top_right(spinda_coords()),
             bottom_left(spinda_coords()),
             bottom_right(spinda_coords()) {}
 
+        /*!
+         * @brief Constructor taking in four coordinate sets.
+         * \param tl Coordinates for top-left spot.
+         * \param tr Coordinates for top-right spot.
+         * \param bl Coordinates for bottom-left spot.
+         * \param br Coordinates for bottom-right spot.
+         */
         PKMN_CONSTEXPR_OR_INLINE spinda_spots(
             const spinda_coords &tl,
             const spinda_coords &tr,
@@ -76,18 +116,28 @@ namespace pkmn { namespace calculations {
            bottom_left(bl),
            bottom_right(br) {}
 
-#ifndef SWIG
+        /*!
+         * @brief Copy constructor.
+         * \param spots Coordinate set whose coordinates to copy.
+         */
         PKMN_CONSTEXPR_OR_INLINE spinda_spots(
-            spinda_coords&& tl,
-            spinda_coords&& tr,
-            spinda_coords&& bl,
-            spinda_coords&& br
-        ): top_left(tl),
-           top_right(tr),
-           bottom_left(bl),
-           bottom_right(br) {}
-#endif
+            const spinda_spots &spots
+        ): top_left(spots.top_left),
+           top_right(spots.top_right),
+           bottom_left(spots.bottom_left),
+           bottom_right(spots.bottom_right) {}
 
+        /*!
+         * @brief Constructor taking in individual parts of coordinates.
+         * \param tl_x X coordinate of top-left spot.
+         * \param tl_y Y coordinate of top-left spot.
+         * \param tr_x X coordinate of top-right spot.
+         * \param tr_y Y coordinate of top-right spot.
+         * \param bl_x X coordinate of bottom-left spot.
+         * \param bl_y Y coordinate of bottom-left spot.
+         * \param br_x X coordinate of bottom-right spot.
+         * \param br_y Y coordinate of bottom-right spot.
+         */
         PKMN_CONSTEXPR_OR_INLINE spinda_spots(
             int tl_x, int tl_y,
             int tr_x, int tr_y,
@@ -98,13 +148,69 @@ namespace pkmn { namespace calculations {
            bottom_left(spinda_coords(bl_x, bl_y)),
            bottom_right(spinda_coords(br_x, br_y)) {}
 
+#ifndef SWIG
+        /*!
+         * @brief Constructor taking in references to individual spots.
+         * \param tl Coordinates for top-left spot.
+         * \param tr Coordinates for top-right spot.
+         * \param bl Coordinates for bottom-left spot.
+         * \param br Coordinates for bottom-right spot.
+         */
+        PKMN_CONSTEXPR_OR_INLINE spinda_spots(
+            spinda_coords&& tl,
+            spinda_coords&& tr,
+            spinda_coords&& bl,
+            spinda_coords&& br
+        ): top_left(tl),
+           top_right(tr),
+           bottom_left(bl),
+           bottom_right(br) {}
+
+        /*!
+         * @brief Move constructor.
+         * \param spots Coordinate set whose coordinates to move.
+         */
+        PKMN_CONSTEXPR_OR_INLINE spinda_spots(
+            spinda_spots&& spots
+        ): top_left(spots.top_left),
+           top_right(spots.top_right),
+           bottom_left(spots.bottom_left),
+           bottom_right(spots.bottom_right) {}
+
+        /*!
+         * @brief Assignment operator.
+         * \param rhs Spots whose member to assign.
+         */
+        PKMN_INLINE spinda_spots& operator=(
+            const spinda_spots &rhs
+        ) {
+            this->top_left = rhs.top_left;
+            this->top_right = rhs.top_right;
+            this->bottom_left = rhs.bottom_left;
+            this->bottom_right = rhs.bottom_right;
+            return *this;
+        }
+#endif
+
+        //! Coordinates of top-left spot.
         spinda_coords top_left;
+
+        //! Coordinates of top-right spot.
         spinda_coords top_right;
+
+        //! Coordinates of bottom-left spot.
         spinda_coords bottom_left;
+
+        //! Coordinates of top-right spot.
         spinda_coords bottom_right;
     };
 
 #ifndef SWIG
+    /*!
+     * @brief Equality check between two sets of Spinda spot coordinates.
+     * \param lhs Coordinates to left of == operator
+     * \param rhs Coordinates to right of == operator
+     */
     PKMN_CONSTEXPR_OR_INLINE bool operator==(
         const spinda_spots &lhs,
         const spinda_spots &rhs
@@ -115,6 +221,11 @@ namespace pkmn { namespace calculations {
                (lhs.bottom_right != rhs.bottom_right);
     }
 
+    /*!
+     * @brief Inequality check between two sets of Spinda spot coordinates.
+     * \param lhs Coordinates to left of != operator
+     * \param rhs Coordinates to right of != operator
+     */
     PKMN_CONSTEXPR_OR_INLINE bool operator!=(
         const spinda_spots &lhs,
         const spinda_spots &rhs
@@ -123,10 +234,28 @@ namespace pkmn { namespace calculations {
     }
 #endif
 
+    /*!
+     * @brief Calculates positions of Spinda's spots in a Generation III sprite.
+     *
+     * The locations of Spinda's four spots are calculated based on the Spinda's
+     * personality. As such, there are 2^32 combinations.
+     *
+     * \param personality Spinda's personality
+     * \returns Locations of Spinda's spots
+     */
     PKMN_API spinda_spots gen3_spinda_spots(
         uint32_t personality
     );
 
+    /*!
+     * @brief Calculates positions of Spinda's spots in a Generation IV-V sprite.
+     *
+     * The locations of Spinda's four spots are calculated based on the Spinda's
+     * personality. As such, there are 2^32 combinations.
+     *
+     * \param personality Spinda's personality
+     * \returns Locations of Spinda's spots
+     */
     PKMN_API spinda_spots nds_spinda_spots(
         uint32_t personality
     );
