af4b1b008c51632e96072979df512f7c53caef63
Nicholas Corgan <n.corgan@gmail.com>
2016-08-09 07:28:03 -0500

    build_info: added missing PKMN_API

--- include/pkmn/build_info.hpp
+++ include/pkmn/build_info.hpp
@@ -7,17 +7,19 @@
 #ifndef INCLUDED_PKMN_BUILD_INFO_HPP
 #define INCLUDED_PKMN_BUILD_INFO_HPP
 
+#include <pkmn/config.hpp>
+
 #include <string>
 
 namespace pkmn { namespace build_info {
 
-    std::string get_boost_version();
+    PKMN_API std::string get_boost_version();
 
-    std::string get_pksav_version();
+    PKMN_API std::string get_pksav_version();
 
-    std::string get_sqlite3_version();
+    PKMN_API std::string get_sqlite3_version();
 
-    std::string get_sqlitecpp_version();
+    PKMN_API std::string get_sqlitecpp_version();
 
 }}
 
