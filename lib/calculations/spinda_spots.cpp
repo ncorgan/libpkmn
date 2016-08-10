bdf735227aeaa3cdc1b8b9752559a9a29f22d3d9
Nicholas Corgan <n.corgan@gmail.com>
2016-08-10 09:47:06 -0500

    calculations: finished Spinda functions

--- lib/calculations/spinda_spots.cpp
+++ lib/calculations/spinda_spots.cpp
@@ -7,6 +7,8 @@
 
 #include <pkmn/calculations/spinda_spots.hpp>
 
+#include <pksav/math/endian.h>
+
 #include <boost/config.hpp>
 
 /*
@@ -15,18 +17,85 @@
 
 namespace pkmn { namespace calculations {
 
+    /*
+     * Helper functions
+     */
+
+    BOOST_STATIC_CONSTEXPR spinda_coords operator+(
+        const spinda_coords &lhs,
+        const spinda_coords &rhs
+    ) {
+        return spinda_coords(
+                   (lhs.x + rhs.x),
+                   (lhs.y + rhs.y)
+               );
+    }
+
+    BOOST_STATIC_CONSTEXPR spinda_spots operator+(
+        const spinda_spots &lhs,
+        const spinda_spots &rhs
+    ) {
+        return spinda_spots(
+                   spinda_coords(lhs.left_ear + rhs.left_ear),
+                   spinda_coords(lhs.right_ear + rhs.right_ear),
+                   spinda_coords(lhs.left_face + rhs.left_face),
+                   spinda_coords(lhs.right_face + rhs.right_face)
+               );
+    }
+
+    BOOST_STATIC_CONSTEXPR spinda_spots get_offset(
+        uint32_t personality
+    ) {
+        return spinda_spots(
+                   spinda_coords(
+                       int( personality & 0x0000000F),
+                       int((personality & 0x000000F0) >> 4)
+                   ),
+                   spinda_coords(
+                       int((personality & 0x0000F000) >> 8),
+                       int((personality & 0x00000F00) >> 12)
+                   ),
+                   spinda_coords(
+                       int((personality & 0x000F0000) >> 16),
+                       int((personality & 0x00F00000) >> 20)
+                   ),
+                   spinda_coords(
+                       int((personality & 0x0F000000) >> 24),
+                       int((personality & 0xF0000000) >> 28)
+                   )
+               );
+    }
+
+    /*
+     * Spot origins
+     */
+
+    BOOST_STATIC_CONSTEXPR spinda_spots gen3_origin(
+        spinda_coords(24,1),  spinda_coords(0,0),
+        spinda_coords(18,19), spinda_coords(6,18)
+    );
+
+    BOOST_STATIC_CONSTEXPR spinda_spots nds_origin(
+        spinda_coords(24,2),  spinda_coords(0,0),
+        spinda_coords(15,18), spinda_coords(3,18)
+    );
+
+    /*
+     * Main functions
+     */
+
     spinda_spots gen3_spinda_spots(
         uint32_t personality
     ) {
-        (void)personality;
-        return spinda_spots();
+        uint32_t pid_le32 = pksav_littleendian32(personality);
+        return (gen3_origin + get_offset(pid_le32));
     }
 
     spinda_spots nds_spinda_spots(
         uint32_t personality
     ) {
-        (void)personality;
-        return spinda_spots();
+        uint32_t pid_le32 = pksav_littleendian32(personality);
+        return (nds_origin + get_offset(pid_le32));
     }
 
 }}
