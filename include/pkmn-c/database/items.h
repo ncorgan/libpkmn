f1b545c290ba88f46266a23153acf0078b5d9e7d
Nicholas Corgan <n.corgan@gmail.com>
2016-08-21 16:17:50 -0500

    C: starting to wrap item_entry

---
+++ include/pkmn-c/database/items.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2016 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+#ifndef PKMN_C_ITEMS_H
+#define PKMN_C_ITEMS_H
+
+#include <pkmn-c/config.h>
+
+#endif /* PKMN_C_ITEMS_H */
---
+++ lib/c/database/items.cpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2016 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+
+#include "cpp_to_c.hpp"
+#include "error_internal.hpp"
+
+#include <pkmn-c/database/items.h>
+
+#include <pkmn/database/item_entry.hpp>
