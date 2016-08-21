9da9692992544d4103df876fdc3014b4fde11854
Nicholas Corgan <n.corgan@gmail.com>
2016-08-21 14:24:25 -0500

    c: starting common C++ -> C functions

--- lib/c/cpp_to_c.cpp
+++ lib/c/cpp_to_c.cpp
@@ -4,3 +4,19 @@
  * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
  * or copy at http://opensource.org/licenses/MIT)
  */
+
+#include "cpp_to_c.hpp"
+
+void std_vector_std_string_to_c_strs(
+    const std::vector<std::string> &vec,
+    char*** c_strs_out,
+    size_t* list_len_out
+) {
+    *c_strs_out = (char**)std::malloc(sizeof(char*) * vec.size());
+    for(size_t i = 0; i < vec.size(); ++i) {
+        (*c_strs_out)[i] = (char*)std::malloc(vec[i].size() + 1);
+        std::strcpy((*c_strs_out)[i], vec[i].c_str());
+        (*c_strs_out)[i][vec[i].size()] = '\0';
+    }
+    *list_len_out = vec.size();
+}
---
+++ lib/c/cpp_to_c.hpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2016 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+
+#include <pkmn/config.hpp>
+
+#include <cstring>
+#include <string>
+#include <vector>
+
+PKMN_INLINE void std_string_to_c_str(
+    const std::string &str,
+    size_t buffer_len,
+    char* c_str_out,
+    size_t* actual_strlen_out
+) {
+    std::strncpy(c_str_out, str.c_str(), buffer_len);
+    *actual_strlen_out = str.size();
+}
+
+void std_vector_std_string_to_c_strs(
+    const std::vector<std::string> &vec,
+    char*** c_strs_out,
+    size_t* list_len_out
+);
