f52be129fa0e77c6970588d7402aa5e9d07744d4
Nicholas Corgan <n.corgan@gmail.com>
2016-08-21 15:45:33 -0500

    C: wrapped build info

--- lib/c/cpp_to_c.hpp
+++ lib/c/cpp_to_c.hpp
@@ -14,20 +14,24 @@
 #include <string>
 #include <vector>
 
-PKMN_INLINE void std_string_to_c_str(
-    const std::string &str,
-    size_t buffer_len,
-    char* c_str_out,
-    size_t* actual_strlen_out
-) {
-    std::strncpy(c_str_out, str.c_str(), buffer_len);
-    *actual_strlen_out = str.size();
-}
+namespace pkmn {
 
-void std_vector_std_string_to_c_strs(
-    const std::vector<std::string> &vec,
-    char*** c_strs_out,
-    size_t* list_len_out
-);
+    PKMN_INLINE void std_string_to_c_str(
+        const std::string &str,
+        char* c_str_out,
+        size_t buffer_len,
+        size_t* actual_strlen_out
+    ) {
+        std::strncpy(c_str_out, str.c_str(), buffer_len);
+        *actual_strlen_out = str.size();
+    }
+
+    void std_vector_std_string_to_c_strs(
+        const std::vector<std::string> &vec,
+        char*** c_strs_out,
+        size_t* list_len_out
+    );
+
+}
 
 #endif /* PKMN_C_CPP_TO_C_HPP */
