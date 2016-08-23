3f2a763442549686b9e5e32ef17a61f407b4bf3f
Nicholas Corgan <n.corgan@gmail.com>
2016-08-22 19:14:06 -0500

    c_lists_test: wrote code that doesn't depend on database branch

--- lib/c/error_internal.hpp
+++ lib/c/error_internal.hpp
@@ -25,37 +25,37 @@ void pkmn_set_error(
         return PKMN_ERROR_NONE; \
     } catch(const std::invalid_argument &e) { \
         pkmn_set_error(e.what()); \
-        return PKMN_INVALID_ARGUMENT; \
+        return PKMN_ERROR_INVALID_ARGUMENT; \
     } catch(const std::domain_error &e) { \
         pkmn_set_error(e.what()); \
-        return PKMN_DOMAIN_ERROR; \
+        return PKMN_ERROR_DOMAIN_ERROR; \
     } catch(const std::length_error &e) { \
         pkmn_set_error(e.what()); \
-        return PKMN_LENGTH_ERROR; \
+        return PKMN_ERROR_LENGTH_ERROR; \
     } catch(const std::out_of_range &e) { \
         pkmn_set_error(e.what()); \
-        return PKMN_OUT_OF_RANGE; \
+        return PKMN_ERROR_OUT_OF_RANGE; \
     } catch(const std::logic_error &e) { \
         pkmn_set_error(e.what()); \
-        return PKMN_LOGIC_ERROR; \
+        return PKMN_ERROR_LOGIC_ERROR; \
     } catch(const std::range_error &e) { \
         pkmn_set_error(e.what()); \
-        return PKMN_RANGE_ERROR; \
+        return PKMN_ERROR_RANGE_ERROR; \
     } catch(const std::overflow_error &e) { \
         pkmn_set_error(e.what()); \
-        return PKMN_OVERFLOW_ERROR; \
+        return PKMN_ERROR_OVERFLOW_ERROR; \
     } catch(const std::underflow_error &e) { \
         pkmn_set_error(e.what()); \
-        return PKMN_UNDERFLOW_ERROR; \
+        return PKMN_ERROR_UNDERFLOW_ERROR; \
     } catch(const std::runtime_error &e) { \
         pkmn_set_error(e.what()); \
-        return PKMN_RUNTIME_ERROR; \
+        return PKMN_ERROR_RUNTIME_ERROR; \
     } catch(const std::exception &e) { \
         pkmn_set_error(e.what()); \
-        return PKMN_STD_EXCEPTION; \
+        return PKMN_ERROR_STD_EXCEPTION; \
     } catch(...) { \
         pkmn_set_error("Unknown error"); \
-        return PKMN_UNKNOWN_ERROR; \
+        return PKMN_ERROR_UNKNOWN_ERROR; \
     } \
 }
 
--- tests/c_lists_test.c
+++ tests/c_lists_test.c
@@ -7,9 +7,189 @@
 
 #include "c_test_common.h"
 
+#include <pkmn-c/database/lists.h>
+
 static void ability_list_test() {
+    pkmn_string_list_t abilities = NULL;
+    size_t list_length = 0;
+    pkmn_error_t error_code = PKMN_ERROR_NONE;
+
+    // Make sure invalid generations return PKMN_ERROR_OUT_OF_RANGE
+    error_code = pkmn_database_ability_list(
+                     0, &abilities,
+                     &list_length
+                 );
+    TEST_ASSERT_EQUAL(error_code, PKMN_ERROR_OUT_OF_RANGE);
+
+    error_code = pkmn_database_ability_list(
+                     6, &abilities,
+                     &list_length
+                 );
+    TEST_ASSERT_EQUAL(error_code, PKMN_ERROR_NONE);
+    TEST_ASSERT_NOT_NULL(abilities);
+    TEST_ASSERT_EQUAL(list_length, 191);
+
+    pkmn_string_list_free(&abilities, list_length);
+}
+
+static void game_list_test() {
+    pkmn_string_list_t games = NULL;
+    size_t list_length = 0;
+    pkmn_error_t error_code = PKMN_ERROR_NONE;
+
+    // Make sure invalid generations return PKMN_ERROR_OUT_OF_RANGE
+    error_code = pkmn_database_game_list(
+                     7, true,
+                     &games,
+                     &list_length
+                 );
+    TEST_ASSERT_EQUAL(error_code, PKMN_ERROR_OUT_OF_RANGE);
+
+    error_code = pkmn_database_game_list(
+                     6, true,
+                     &games,
+                     &list_length
+                 );
+    TEST_ASSERT_EQUAL(error_code, PKMN_ERROR_NONE);
+    TEST_ASSERT_NOT_NULL(games);
+    TEST_ASSERT_EQUAL(list_length, 26);
+
+    pkmn_string_list_free(&games, list_length);
+}
+
+/*
+ * TODO: rest when database branch merged in
+ *  * Check invalid params where appropriate
+ *  * Check for non-null string lists
+ *  * Check string list lengths
+ */
+
+static void item_list_test() {
+    pkmn_string_list_t items = NULL;
+    size_t list_length = 0;
+    pkmn_error_t error_code = PKMN_ERROR_NONE;
+
+    error_code = pkmn_database_item_list(
+                     "Red",
+                     &items,
+                     &list_length
+                 );
+    TEST_ASSERT_EQUAL(error_code, PKMN_ERROR_NONE);
+
+    pkmn_string_list_free(&items, list_length);
+}
+
+static void location_list_test() {
+    pkmn_string_list_t locations = NULL;
+    size_t list_length = 0;
+    pkmn_error_t error_code = PKMN_ERROR_NONE;
+
+    error_code = pkmn_database_location_list(
+                     "Red", false,
+                     &locations,
+                     &list_length
+                 );
+    TEST_ASSERT_EQUAL(error_code, PKMN_ERROR_NONE);
+
+    pkmn_string_list_free(&locations, list_length);
+}
+
+static void move_list_test() {
+    pkmn_string_list_t moves = NULL;
+    size_t list_length = 0;
+    pkmn_error_t error_code = PKMN_ERROR_NONE;
+
+    error_code = pkmn_database_move_list(
+                     "Red",
+                     &moves,
+                     &list_length
+                 );
+    TEST_ASSERT_EQUAL(error_code, PKMN_ERROR_NONE);
+
+    pkmn_string_list_free(&moves, list_length);
+}
+
+static void nature_list_test() {
+    pkmn_string_list_t natures = NULL;
+    size_t list_length = 0;
+    pkmn_error_t error_code = PKMN_ERROR_NONE;
+
+    error_code = pkmn_database_nature_list(
+                     &natures,
+                     &list_length
+                 );
+    TEST_ASSERT_EQUAL(error_code, PKMN_ERROR_NONE);
+
+    pkmn_string_list_free(&natures, list_length);
+}
+
+static void pokemon_list_test() {
+    pkmn_string_list_t pokemon = NULL;
+    size_t list_length = 0;
+    pkmn_error_t error_code = PKMN_ERROR_NONE;
+
+    error_code = pkmn_database_pokemon_list(
+                     1, true,
+                     &pokemon,
+                     &list_length
+                 );
+    TEST_ASSERT_EQUAL(error_code, PKMN_ERROR_NONE);
+
+    pkmn_string_list_free(&pokemon, list_length);
+}
+
+static void region_list_test() {
+    pkmn_string_list_t regions = NULL;
+    size_t list_length = 0;
+    pkmn_error_t error_code = PKMN_ERROR_NONE;
+
+    error_code = pkmn_database_region_list(
+                     &regions,
+                     &list_length
+                 );
+    TEST_ASSERT_EQUAL(error_code, PKMN_ERROR_NONE);
+
+    pkmn_string_list_free(&regions, list_length);
+}
+
+static void ribbon_list_test() {
+    pkmn_string_list_t ribbons = NULL;
+    size_t list_length = 0;
+    pkmn_error_t error_code = PKMN_ERROR_NONE;
+
+    error_code = pkmn_database_ribbon_list(
+                     1,
+                     &ribbons,
+                     &list_length
+                 );
+    TEST_ASSERT_EQUAL(error_code, PKMN_ERROR_NONE);
+
+    pkmn_string_list_free(&ribbons, list_length);
+}
+
+static void super_training_medal_list_test() {
+    pkmn_string_list_t super_training_medals = NULL;
+    size_t list_length = 0;
+    pkmn_error_t error_code = PKMN_ERROR_NONE;
+
+    error_code = pkmn_database_super_training_medal_list(
+                     &super_training_medals,
+                     &list_length
+                 );
+    TEST_ASSERT_EQUAL(error_code, PKMN_ERROR_NONE);
+
+    pkmn_string_list_free(&super_training_medals, list_length);
 }
 
 PKMN_C_TEST_MAIN(
     PKMN_C_TEST(ability_list_test)
+    PKMN_C_TEST(game_list_test)
+    PKMN_C_TEST(item_list_test)
+    PKMN_C_TEST(location_list_test)
+    PKMN_C_TEST(move_list_test)
+    PKMN_C_TEST(nature_list_test)
+    PKMN_C_TEST(pokemon_list_test)
+    PKMN_C_TEST(region_list_test)
+    PKMN_C_TEST(ribbon_list_test)
+    PKMN_C_TEST(super_training_medal_list_test)
 )
