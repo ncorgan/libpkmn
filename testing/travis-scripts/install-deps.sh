4c41a1e651c6aee815d782b85aaa10dbff4e932f
Nicholas Corgan <n.corgan@gmail.com>
2016-11-19 18:59:17 -0600

    travis: install Pip and CppHeaderParser

--- testing/travis-scripts/install-deps.sh
+++ testing/travis-scripts/install-deps.sh
@@ -17,5 +17,6 @@ else
     sudo apt-get -y update
     sudo apt-get -y install build-essential cmake clang cppcheck valgrind \
 	                    doxygen libpython-dev python-ply libboost-all-dev \
-			    swig
+			    swig python-pip
+    sudo pip install CppHeaderParser
 fi
