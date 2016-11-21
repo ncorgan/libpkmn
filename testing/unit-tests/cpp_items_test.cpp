0ccf0b19e7ae825fd6b674a4a402ac0832b40fe2
Nicholas Corgan <n.corgan@gmail.com>
2016-11-20 19:25:39 -0600

    Starting Generation II item tests

--- testing/unit-tests/cpp_items_test.cpp
+++ testing/unit-tests/cpp_items_test.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <pkmntest/gen1_items_tests.hpp>
+#include <pkmntest/gen2_items_tests.hpp>
 
 #include <boost/test/unit_test.hpp>
 
@@ -22,4 +23,94 @@ BOOST_AUTO_TEST_CASE(gen1_pc_test) {
 BOOST_AUTO_TEST_CASE(gen1_item_bag_test) {
     pkmn::item_bag::sptr bag = pkmn::item_bag::make("Red");
     pkmntest::gen1_item_bag_test(bag);
+}
+
+BOOST_AUTO_TEST_CASE(gen2_gold_item_pocket_test) {
+    pkmn::item_list::sptr item_pocket = pkmn::item_list::make("Items", "Gold");
+    pkmntest::gen2_item_pocket_test(item_pocket, "Gold");
+}
+
+BOOST_AUTO_TEST_CASE(gen2_gold_key_item_pocket_test) {
+    pkmn::item_list::sptr key_item_pocket = pkmn::item_list::make("KeyItems", "Gold");
+    pkmntest::gen2_key_item_pocket_test(key_item_pocket, "Gold");
+}
+
+BOOST_AUTO_TEST_CASE(gen2_gold_ball_pocket_test) {
+    pkmn::item_list::sptr ball_pocket = pkmn::item_list::make("Balls", "Gold");
+    pkmntest::gen2_ball_pocket_test(ball_pocket, "Gold");
+}
+
+BOOST_AUTO_TEST_CASE(gen2_gold_tmhm_pocket_test) {
+    pkmn::item_list::sptr tmhm_pocket = pkmn::item_list::make("TM/HM", "Gold");
+    pkmntest::gen2_tmhm_pocket_test(tmhm_pocket, "Gold");
+}
+
+BOOST_AUTO_TEST_CASE(gen2_gold_pc_test) {
+    pkmn::item_list::sptr pc = pkmn::item_list::make("PC", "Gold");
+    pkmntest::gen2_pc_test(pc, "Gold");
+}
+
+BOOST_AUTO_TEST_CASE(gen2_gold_item_bag_test) {
+    pkmn::item_bag::sptr bag = pkmn::item_bag::make("Gold");
+    pkmntest::gen2_item_bag_test(bag, "Gold");
+}
+
+BOOST_AUTO_TEST_CASE(gen2_silver_item_pocket_test) {
+    pkmn::item_list::sptr item_pocket = pkmn::item_list::make("Items", "Silver");
+    pkmntest::gen2_item_pocket_test(item_pocket, "Silver");
+}
+
+BOOST_AUTO_TEST_CASE(gen2_silver_key_item_pocket_test) {
+    pkmn::item_list::sptr key_item_pocket = pkmn::item_list::make("KeyItems", "Silver");
+    pkmntest::gen2_key_item_pocket_test(key_item_pocket, "Silver");
+}
+
+BOOST_AUTO_TEST_CASE(gen2_silver_ball_pocket_test) {
+    pkmn::item_list::sptr ball_pocket = pkmn::item_list::make("Balls", "Silver");
+    pkmntest::gen2_ball_pocket_test(ball_pocket, "Silver");
+}
+
+BOOST_AUTO_TEST_CASE(gen2_silver_tmhm_pocket_test) {
+    pkmn::item_list::sptr tmhm_pocket = pkmn::item_list::make("TM/HM", "Silver");
+    pkmntest::gen2_tmhm_pocket_test(tmhm_pocket, "Silver");
+}
+
+BOOST_AUTO_TEST_CASE(gen2_silver_pc_test) {
+    pkmn::item_list::sptr pc = pkmn::item_list::make("PC", "Silver");
+    pkmntest::gen2_pc_test(pc, "Silver");
+}
+
+BOOST_AUTO_TEST_CASE(gen2_silver_item_bag_test) {
+    pkmn::item_bag::sptr bag = pkmn::item_bag::make("Silver");
+    pkmntest::gen2_item_bag_test(bag, "Silver");
+}
+
+BOOST_AUTO_TEST_CASE(gen2_crystal_item_pocket_test) {
+    pkmn::item_list::sptr item_pocket = pkmn::item_list::make("Items", "Crystal");
+    pkmntest::gen2_item_pocket_test(item_pocket, "Crystal");
+}
+
+BOOST_AUTO_TEST_CASE(gen2_crystal_key_item_pocket_test) {
+    pkmn::item_list::sptr key_item_pocket = pkmn::item_list::make("KeyItems", "Crystal");
+    pkmntest::gen2_key_item_pocket_test(key_item_pocket, "Crystal");
+}
+
+BOOST_AUTO_TEST_CASE(gen2_crystal_ball_pocket_test) {
+    pkmn::item_list::sptr ball_pocket = pkmn::item_list::make("Balls", "Crystal");
+    pkmntest::gen2_ball_pocket_test(ball_pocket, "Crystal");
+}
+
+BOOST_AUTO_TEST_CASE(gen2_crystal_tmhm_pocket_test) {
+    pkmn::item_list::sptr tmhm_pocket = pkmn::item_list::make("TM/HM", "Crystal");
+    pkmntest::gen2_tmhm_pocket_test(tmhm_pocket, "Crystal");
+}
+
+BOOST_AUTO_TEST_CASE(gen2_crystal_pc_test) {
+    pkmn::item_list::sptr pc = pkmn::item_list::make("PC", "Crystal");
+    pkmntest::gen2_pc_test(pc, "Crystal");
+}
+
+BOOST_AUTO_TEST_CASE(gen2_crystal_item_bag_test) {
+    pkmn::item_bag::sptr bag = pkmn::item_bag::make("Crystal");
+    pkmntest::gen2_item_bag_test(bag, "Crystal");
 }
