ba2ad0c5363d4a3cf7fa44c0eb33ec96fe559266
Nicholas Corgan <n.corgan@gmail.com>
2016-11-20 19:25:39 -0600

    Starting Generation II item tests

---
+++ testing/unit-tests/pkmntest/cpp/gen2_items_tests.cpp
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2016 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+
+#include <pkmntest/gen2_items_tests.hpp>
+
+// Don't create the main in a library
+#undef BOOST_TEST_MAIN
+
+#include <boost/test/unit_test.hpp>
+
+#include <iostream>
+
+namespace pkmntest {
+
+    void gen2_item_pocket_test(
+        pkmn::item_list::sptr item_pocket,
+        const std::string &game
+    ) {
+        BOOST_CHECK_EQUAL(item_pocket->get_name(), "Items");
+        BOOST_CHECK_EQUAL(item_pocket->get_game(), game);
+    }
+
+    void gen2_key_item_pocket_test(
+        pkmn::item_list::sptr key_item_pocket,
+        const std::string &game
+    ) {
+        BOOST_CHECK_EQUAL(key_item_pocket->get_name(), "KeyItems");
+        BOOST_CHECK_EQUAL(key_item_pocket->get_game(), game);
+
+        if(game == "Crystal") {
+            key_item_pocket->add("GS Ball", 1);
+        } else {
+            BOOST_CHECK_THROW(
+                key_item_pocket->add("GS Ball", 1);
+            , std::invalid_argument);
+        }
+    }
+
+    void gen2_ball_pocket_test(
+        pkmn::item_list::sptr ball_pocket,
+        const std::string &game
+    ) {
+        BOOST_CHECK_EQUAL(ball_pocket->get_name(), "Balls");
+        BOOST_CHECK_EQUAL(ball_pocket->get_game(), game);
+    }
+
+    void gen2_tmhm_pocket_test(
+        pkmn::item_list::sptr tmhm_pocket,
+        const std::string &game
+    ) {
+        BOOST_CHECK_EQUAL(tmhm_pocket->get_name(), "TM/HM");
+        BOOST_CHECK_EQUAL(tmhm_pocket->get_game(), game);
+
+        for(int i = 1; i <= 50; ++i) {
+            char name[5] = "";
+            std::snprintf(name, sizeof(name), "TM%02d", i);
+            tmhm_pocket->add(name, 50);
+            BOOST_CHECK_EQUAL(tmhm_pocket->get_num_items(), i);
+        }
+        for(int i = 50; i >= 1; --i) {
+            char name[5];
+            std::snprintf(name, sizeof(name), "TM%02d", i);
+            tmhm_pocket->remove(name, 50);
+            BOOST_CHECK_EQUAL(tmhm_pocket->get_num_items(), i-1);
+        }
+
+        for(int i = 1; i <= 7; ++i) {
+            char name[5];
+            std::snprintf(name, sizeof(name), "HM%02d", i);
+            tmhm_pocket->add(name, 1);
+            BOOST_CHECK_EQUAL(tmhm_pocket->get_num_items(), i);
+        }
+        for(int i = 7; i >= 1; --i) {
+            char name[5];
+            std::snprintf(name, sizeof(name), "HM%02d", i);
+            tmhm_pocket->remove(name, 1);
+            BOOST_CHECK_EQUAL(tmhm_pocket->get_num_items(), i-1);
+        }
+    }
+
+    void gen2_pc_test(
+        pkmn::item_list::sptr pc,
+        const std::string &game
+    ) {
+        BOOST_CHECK_EQUAL(pc->get_name(), "PC");
+        BOOST_CHECK_EQUAL(pc->get_game(), game);
+    }
+
+    void gen2_item_bag_test(
+        pkmn::item_bag::sptr bag,
+        const std::string &game
+    ) {
+        BOOST_CHECK_EQUAL(bag->get_game(), game);
+
+        const pkmn::item_pockets_t& pockets = bag->get_pockets();
+        BOOST_CHECK_EQUAL(pockets.size(), 4);
+        gen2_item_pocket_test(pockets.at("Items"), game);
+        gen2_key_item_pocket_test(pockets.at("KeyItems"), game);
+        gen2_ball_pocket_test(pockets.at("Balls"), game);
+        gen2_tmhm_pocket_test(pockets.at("TM/HM"), game);
+    }
+
+}
