942cf341425011148e9e3bea095978d49f886c58
Nicholas Corgan <n.corgan@gmail.com>
2016-11-21 21:04:15 -0600

    Starting C Generation II item test

--- testing/unit-tests/c_items_test.c
+++ testing/unit-tests/c_items_test.c
@@ -8,6 +8,7 @@
 #include "c_test_common.h"
 
 #include <pkmntest-c/gen1_items_tests.h>
+#include <pkmntest-c/gen2_items_tests.h>
 
 #include <pkmn.h>
 
@@ -143,14 +144,205 @@ static void gen1_yellow_item_bag_test() {
     TEST_ASSERT_NULL(bag);
 }
 
+// Gold
+
+static void gen2_gold_item_pocket_test() {
+    pkmn_item_list_handle_t item_pocket = NULL;
+    TEST_ASSERT_EQUAL(pkmn_item_list_make(
+                          &item_pocket, "Items", "Gold"
+                      ), PKMN_ERROR_NONE);
+
+    pkmntest_gen2_item_pocket_test(item_pocket, "Gold");
+}
+
+static void gen2_gold_key_item_pocket_test() {
+    pkmn_item_list_handle_t key_item_pocket = NULL;
+    TEST_ASSERT_EQUAL(pkmn_item_list_make(
+                          &key_item_pocket, "KeyItems", "Gold"
+                      ), PKMN_ERROR_NONE);
+
+    pkmntest_gen2_key_item_pocket_test(key_item_pocket, "Gold");
+}
+
+static void gen2_gold_ball_pocket_test() {
+    pkmn_item_list_handle_t ball_pocket = NULL;
+    TEST_ASSERT_EQUAL(pkmn_item_list_make(
+                          &ball_pocket, "Balls", "Gold"
+                      ), PKMN_ERROR_NONE);
+
+    pkmntest_gen2_ball_pocket_test(ball_pocket, "Gold");
+}
+
+static void gen2_gold_tmhm_pocket_test() {
+    pkmn_item_list_handle_t tmhm_pocket = NULL;
+    TEST_ASSERT_EQUAL(pkmn_item_list_make(
+                          &tmhm_pocket, "TM/HM", "Gold"
+                      ), PKMN_ERROR_NONE);
+
+    pkmntest_gen2_tmhm_pocket_test(tmhm_pocket, "Gold");
+}
+
+static void gen2_gold_pc_test() {
+    pkmn_item_list_handle_t pc = NULL;
+    TEST_ASSERT_EQUAL(pkmn_item_list_make(
+                          &pc, "PC", "Gold"
+                      ), PKMN_ERROR_NONE);
+
+    pkmntest_gen2_pc_test(pc, "Gold");
+}
+
+static void gen2_gold_item_bag_test() {
+    pkmn_item_bag_handle_t item_bag = NULL;
+    TEST_ASSERT_EQUAL(pkmn_item_bag_make(
+                          &item_bag, "Gold"
+                      ), PKMN_ERROR_NONE);
+
+    pkmntest_gen2_item_bag_test(item_bag, "Gold");
+}
+
+// Silver
+
+static void gen2_silver_item_pocket_test() {
+    pkmn_item_list_handle_t item_pocket = NULL;
+    TEST_ASSERT_EQUAL(pkmn_item_list_make(
+                          &item_pocket, "Items", "Silver"
+                      ), PKMN_ERROR_NONE);
+
+    pkmntest_gen2_item_pocket_test(item_pocket, "Silver");
+}
+
+static void gen2_silver_key_item_pocket_test() {
+    pkmn_item_list_handle_t key_item_pocket = NULL;
+    TEST_ASSERT_EQUAL(pkmn_item_list_make(
+                          &key_item_pocket, "KeyItems", "Silver"
+                      ), PKMN_ERROR_NONE);
+
+    pkmntest_gen2_key_item_pocket_test(key_item_pocket, "Silver");
+}
+
+static void gen2_silver_ball_pocket_test() {
+    pkmn_item_list_handle_t ball_pocket = NULL;
+    TEST_ASSERT_EQUAL(pkmn_item_list_make(
+                          &ball_pocket, "Balls", "Silver"
+                      ), PKMN_ERROR_NONE);
+
+    pkmntest_gen2_ball_pocket_test(ball_pocket, "Silver");
+}
+
+static void gen2_silver_tmhm_pocket_test() {
+    pkmn_item_list_handle_t tmhm_pocket = NULL;
+    TEST_ASSERT_EQUAL(pkmn_item_list_make(
+                          &tmhm_pocket, "TM/HM", "Silver"
+                      ), PKMN_ERROR_NONE);
+
+    pkmntest_gen2_tmhm_pocket_test(tmhm_pocket, "Silver");
+}
+
+static void gen2_silver_pc_test() {
+    pkmn_item_list_handle_t pc = NULL;
+    TEST_ASSERT_EQUAL(pkmn_item_list_make(
+                          &pc, "PC", "Silver"
+                      ), PKMN_ERROR_NONE);
+
+    pkmntest_gen2_pc_test(pc, "Silver");
+}
+
+static void gen2_silver_item_bag_test() {
+    pkmn_item_bag_handle_t item_bag = NULL;
+    TEST_ASSERT_EQUAL(pkmn_item_bag_make(
+                          &item_bag, "Silver"
+                      ), PKMN_ERROR_NONE);
+
+    pkmntest_gen2_item_bag_test(item_bag, "Silver");
+}
+
+// Crystal
+
+static void gen2_crystal_item_pocket_test() {
+    pkmn_item_list_handle_t item_pocket = NULL;
+    TEST_ASSERT_EQUAL(pkmn_item_list_make(
+                          &item_pocket, "Items", "Crystal"
+                      ), PKMN_ERROR_NONE);
+
+    pkmntest_gen2_item_pocket_test(item_pocket, "Crystal");
+}
+
+static void gen2_crystal_key_item_pocket_test() {
+    pkmn_item_list_handle_t key_item_pocket = NULL;
+    TEST_ASSERT_EQUAL(pkmn_item_list_make(
+                          &key_item_pocket, "KeyItems", "Crystal"
+                      ), PKMN_ERROR_NONE);
+
+    pkmntest_gen2_key_item_pocket_test(key_item_pocket, "Crystal");
+}
+
+static void gen2_crystal_ball_pocket_test() {
+    pkmn_item_list_handle_t ball_pocket = NULL;
+    TEST_ASSERT_EQUAL(pkmn_item_list_make(
+                          &ball_pocket, "Balls", "Crystal"
+                      ), PKMN_ERROR_NONE);
+
+    pkmntest_gen2_ball_pocket_test(ball_pocket, "Crystal");
+}
+
+static void gen2_crystal_tmhm_pocket_test() {
+    pkmn_item_list_handle_t tmhm_pocket = NULL;
+    TEST_ASSERT_EQUAL(pkmn_item_list_make(
+                          &tmhm_pocket, "TM/HM", "Crystal"
+                      ), PKMN_ERROR_NONE);
+
+    pkmntest_gen2_tmhm_pocket_test(tmhm_pocket, "Crystal");
+}
+
+static void gen2_crystal_pc_test() {
+    pkmn_item_list_handle_t pc = NULL;
+    TEST_ASSERT_EQUAL(pkmn_item_list_make(
+                          &pc, "PC", "Crystal"
+                      ), PKMN_ERROR_NONE);
+
+    pkmntest_gen2_pc_test(pc, "Crystal");
+}
+
+static void gen2_crystal_item_bag_test() {
+    pkmn_item_bag_handle_t item_bag = NULL;
+    TEST_ASSERT_EQUAL(pkmn_item_bag_make(
+                          &item_bag, "Crystal"
+                      ), PKMN_ERROR_NONE);
+
+    pkmntest_gen2_item_bag_test(item_bag, "Crystal");
+}
+
 PKMN_C_TEST_MAIN(
     PKMN_C_TEST(gen1_red_item_list_test)
     PKMN_C_TEST(gen1_red_item_pc_test)
     PKMN_C_TEST(gen1_red_item_bag_test)
+
     PKMN_C_TEST(gen1_blue_item_list_test)
     PKMN_C_TEST(gen1_blue_item_pc_test)
     PKMN_C_TEST(gen1_blue_item_bag_test)
+
     PKMN_C_TEST(gen1_yellow_item_list_test)
     PKMN_C_TEST(gen1_yellow_item_pc_test)
     PKMN_C_TEST(gen1_yellow_item_bag_test)
+
+    PKMN_C_TEST(gen2_gold_item_pocket_test)
+    PKMN_C_TEST(gen2_gold_key_item_pocket_test)
+    PKMN_C_TEST(gen2_gold_ball_pocket_test)
+    PKMN_C_TEST(gen2_gold_tmhm_pocket_test)
+    PKMN_C_TEST(gen2_gold_pc_test)
+    PKMN_C_TEST(gen2_gold_item_bag_test)
+
+    PKMN_C_TEST(gen2_silver_item_pocket_test)
+    PKMN_C_TEST(gen2_silver_key_item_pocket_test)
+    PKMN_C_TEST(gen2_silver_ball_pocket_test)
+    PKMN_C_TEST(gen2_silver_tmhm_pocket_test)
+    PKMN_C_TEST(gen2_silver_pc_test)
+    PKMN_C_TEST(gen2_silver_item_bag_test)
+
+    PKMN_C_TEST(gen2_crystal_item_pocket_test)
+    PKMN_C_TEST(gen2_crystal_key_item_pocket_test)
+    PKMN_C_TEST(gen2_crystal_ball_pocket_test)
+    PKMN_C_TEST(gen2_crystal_tmhm_pocket_test)
+    PKMN_C_TEST(gen2_crystal_pc_test)
+    PKMN_C_TEST(gen2_crystal_item_bag_test)
 )
---
+++ testing/unit-tests/pkmntest/c/gen2_items_tests.c
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2016 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+
+#include <pkmntest-c/gen2_items_tests.h>
+
+#include <unity.h>
+
+#include <string.h>
+
+#define STRBUFFER_LEN 1024
+
+void pkmntest_gen2_item_pocket_test(
+    pkmn_item_list_handle_t item_pocket,
+    const char* game
+) {
+    (void)item_pocket;
+    (void)game;
+}
+
+void pkmntest_gen2_key_item_pocket_test(
+    pkmn_item_list_handle_t key_item_pocket,
+    const char* game
+) {
+    (void)key_item_pocket;
+    (void)game;
+}
+
+void pkmntest_gen2_ball_pocket_test(
+    pkmn_item_list_handle_t ball_pocket,
+    const char* game
+) {
+    (void)ball_pocket;
+    (void)game;
+}
+
+void pkmntest_gen2_tmhm_pocket_test(
+    pkmn_item_list_handle_t tmhm_pocket,
+    const char* game
+) {
+    (void)tmhm_pocket;
+    (void)game;
+}
+
+void pkmntest_gen2_pc_test(
+    pkmn_item_list_handle_t pc,
+    const char* game
+) {
+    (void)pc;
+    (void)game;
+}
+
+void pkmntest_gen2_item_bag_test(
+    pkmn_item_bag_handle_t bag,
+    const char* game
+) {
+    (void)bag;
+    (void)game;
+}
