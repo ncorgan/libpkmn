dcf35a3654e1ced5ff581b98e20ef645b9d9074e
Nicholas Corgan <n.corgan@gmail.com>
2016-11-22 19:33:17 -0600

    Starting unit test for C++ to C conversions

---
+++ testing/unit-tests/cpp_to_c_test.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2016 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(exception_to_error_code_test) {
+}
