94c247664ebcacddff533f4c002c84388a6099af
Nicholas Corgan <n.corgan@gmail.com>
2016-11-24 15:13:29 -0600

    appveyor: specify paths

--- testing/appveyor-scripts/build-test.bat
+++ testing/appveyor-scripts/build-test.bat
@@ -6,17 +6,19 @@ mkdir c:\projects\libpkmn\test-env\build
 cd c:\projects\libpkmn\test-env\build
 if not !ERRORLEVEL!==0 goto fail
 
-cmake -G "%CMAKE_DASH_G%" -DCMAKE_BUILD_TYPE=Release ..\..
+cmake -G %CMAKE_GENERATOR_NAME% ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DBOOST_ROOT="%BOOST_ROOT%" ^
+    -DBOOST_LIBRARYDIR="%BOOST_ROOT%\lib%BITNESS%-msvc-14.0" ^
+    -DPYTHON_EXECUTABLE="%PYTHON_ROOT%\python.exe" ^
+    ..\..
 if not !ERRORLEVEL!==0 goto fail
 
 msbuild /p:configuration=Release ALL_BUILD.vcxproj
 if not !ERRORLEVEL!==0 goto fail
 
-:: Don't run anything when cross-compiling
-if "x%CMAKE_DASH_G:ARM=%"=="x%CMAKE_DASH_G%" (
-    ctest --output-on-failure
-    if not !ERRORLEVEL!==0 goto fail
-)
+ctest --output-on-failure
+if not !ERRORLEVEL!==0 goto fail
 
 goto pass
 
