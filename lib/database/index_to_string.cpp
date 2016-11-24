6fc7d4b27c6868997741fb773cbcbce792656b63
Nicholas Corgan <n.corgan@gmail.com>
2016-11-24 11:36:39 -0600

    database: brought back index_to_string

---
+++ lib/database/index_to_string.cpp
@@ -0,0 +1,157 @@
+/*
+ * Copyright (c) 2016 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+
+#include "database_common.hpp"
+#include "id_to_string.hpp"
+
+#include <boost/config.hpp>
+
+namespace pkmn { namespace database {
+
+    static pkmn::database::sptr _db;
+
+    std::string game_index_to_name(
+        int game_index
+    ) {
+        // Connect to database
+        pkmn::database::get_connection(_db);
+
+        static BOOST_CONSTEXPR const char* query = \
+            "SELECT name FROM version_names WHERE local_language_id=9 AND "
+            "version_id=(SELECT version_id IN version_game_indices WHERE "
+            "game_index=?)";
+
+        return pkmn::database::query_db_bind1<std::string, int>(
+                   _db, query, game_index
+               );
+    }
+
+    int game_name_to_index(
+        const std::string &game_name
+    ) {
+        // Connect to database
+        pkmn::database::get_connection(_db);
+
+        static BOOST_CONSTEXPR const char* query = \
+            "SELECT game_index FROM version_game_indices WHERE version_id="
+            "(SELECT version_id FROM version_names WHERE name=?)";
+
+        return pkmn::database::query_db_bind1<int, const std::string&>(
+                   _db, query, game_name
+               );
+    }
+
+    std::string item_index_to_name(
+        int item_index,
+        int game_id
+    ) {
+        // Connect to database
+        pkmn::database::get_connection(_db);
+
+        (void)item_index;
+        (void)game_id;
+        return "";
+    }
+
+    int item_name_to_index(
+        const std::string &item_name,
+        int game_id
+    ) {
+        // Connect to database
+        pkmn::database::get_connection(_db);
+
+        (void)item_name;
+        (void)game_id;
+        return 0;
+    }
+
+    std::string location_index_to_name(
+        int location_index,
+        int game_id
+    ) {
+        // Connect to database
+        pkmn::database::get_connection(_db);
+
+        (void)location_index;
+        (void)game_id;
+        return "";
+    }
+
+    int location_name_to_index(
+        const std::string &location_name,
+        int game_id
+    ) {
+        // Connect to database
+        pkmn::database::get_connection(_db);
+
+        (void)location_name;
+        (void)game_id;
+        return 0;
+    }
+
+    std::string nature_index_to_name(
+        int nature_index
+    ) {
+        // Connect to database
+        pkmn::database::get_connection(_db);
+
+        static BOOST_CONSTEXPR const char* query = \
+            "SELECT name FROM nature_names WHERE local_language_id=9 "
+            "AND nature_id=(SELECT id FROM natures WHERE game_index=?)";
+
+        return pkmn::database::query_db_bind1<std::string, int>(
+                   _db, query, nature_index
+               );
+    }
+
+    int nature_name_to_index(
+        const std::string &nature_name
+    ) {
+        // Connect to database
+        pkmn::database::get_connection(_db);
+
+        static BOOST_CONSTEXPR const char* query = \
+            "SELECT game_index FROM natures WHERE id="
+            "(SELECT nature_id FROM nature_names WHERE name=?)";
+
+        return pkmn::database::query_db_bind1<int, const std::string&>(
+                   _db, query, nature_name
+               );
+    }
+
+    std::string pokemon_index_to_name(
+        int pokemon_index,
+        int game_id
+    ) {
+        // Connect to database
+        pkmn::database::get_connection(_db);
+
+        static BOOST_CONSTEXPR const char* query = \
+            "SELECT name FROM pokemon_species_names WHERE pokemon_species_id="
+            "(SELECT species_id FROM pokemon WHERE id=(SELECT pokemon_id FROM "
+            "pokemon_game_indices WHERE game_index=? AND version_id=?))";
+
+        return pkmn::database::query_db_bind2<std::string, int, int>(
+                   _db, query, pokemon_index, game_id
+               );
+    }
+
+    int pokemon_name_to_index(
+        const std::string &pokemon_name,
+        const std::string &form_name,
+        int game_id
+    ) {
+        // Connect to database
+        pkmn::database::get_connection(_db);
+
+        (void)pokemon_name;
+        (void)form_name;
+        (void)game_id;
+        return 0;
+    }
+
+}}
---
+++ lib/database/index_to_string.hpp
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2016 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+#ifndef PKMN_DATABASE_INDEX_TO_STRING_HPP
+#define PKMN_DATABASE_INDEX_TO_STRING_HPP
+
+#include <string>
+
+/*
+ * These functions convert between in-game indices and their corresponding
+ * strings.
+ *
+ * Doing this in a single transaction is significantly faster than passing
+ * *_index_to_id into *_id_to_name, and vice versa.
+ */
+
+namespace pkmn { namespace database {
+
+    /*
+     * Games
+     */
+
+    std::string game_index_to_name(
+        int game_index
+    );
+
+    int game_name_to_index(
+        const std::string &game_name
+    );
+
+    /*
+     * Items
+     */
+
+    std::string item_index_to_name(
+        int item_index,
+        int game_id
+    );
+
+    int item_name_to_index(
+        const std::string &item_name,
+        int game_id
+    );
+
+    /*
+     * Locations
+     */
+
+    std::string location_index_to_name(
+        int location_index,
+        int game_id
+    );
+
+    int location_name_to_index(
+        const std::string &location_name,
+        int game_id
+    );
+
+    /*
+     * Natures
+     */
+
+    std::string nature_index_to_name(
+        int nature_index
+    );
+
+    int nature_name_to_index(
+        const std::string &nature_name
+    );
+
+    /*
+     * Pokémon
+     */
+
+    std::string pokemon_index_to_name(
+        int pokemon_index,
+        int game_id
+    );
+
+    int pokemon_name_to_index(
+        const std::string &pokemon_name,
+        const std::string &form_name,
+        int game_id
+    );
+
+}}
+
+#endif /* PKMN_DATABASE_INDEX_TO_STRING_HPP */
