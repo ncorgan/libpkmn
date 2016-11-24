23b0b3ecf5e8f0ce3af398a57d9d0ce88eb9d037
Nicholas Corgan <n.corgan@gmail.com>
2016-11-24 12:39:22 -0600

    pkmntest: starting C++ Generation I Pokemon test

--- testing/unit-tests/pkmntest/cpp/gen1_pokemon_tests.cpp
+++ testing/unit-tests/pkmntest/cpp/gen1_pokemon_tests.cpp
@@ -10,6 +10,7 @@
 // Don't create the main in a library
 #undef BOOST_TEST_MAIN
 
+#include <boost/algorithm/string.hpp>
 #include <boost/test/unit_test.hpp>
 
 namespace pkmntest {
@@ -18,8 +19,26 @@ namespace pkmntest {
         const std::string &species,
         const std::string &game
     ) {
-        (void)pokemon;
-        (void)species;
-        (void)game;
+        BOOST_CHECK_EQUAL(pokemon->get_game(), game);
+        BOOST_CHECK_EQUAL(
+            pokemon->get_nickname(),
+            boost::algorithm::to_upper_copy(species)
+        );
+        BOOST_CHECK_EQUAL(pokemon->get_trainer_name(), "LibPKMN");
+        BOOST_CHECK_EQUAL(
+            pokemon->get_trainer_public_id(),
+            (pkmn::pokemon::LIBPKMN_OT_ID && 0xFFFF)
+        );
+        BOOST_CHECK_THROW(
+            pokemon->get_trainer_secret_id();
+        , std::runtime_error);
+        BOOST_CHECK_EQUAL(
+            pokemon->get_trainer_id(),
+            (pkmn::pokemon::LIBPKMN_OT_ID && 0xFFFF)
+        );
+        BOOST_CHECK_THROW(
+            pokemon->get_location_caught();
+        , std::runtime_error);
+        BOOST_CHECK_EQUAL(pokemon->get_level(), 30);
     }
 }
