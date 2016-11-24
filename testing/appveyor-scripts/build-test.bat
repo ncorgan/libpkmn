0ea04b03ff04b82cffb92d175f4a63d92b9ea4db
Nicholas Corgan <n.corgan@gmail.com>
2016-11-24 14:58:49 -0600

    Copied Appveyor stuff from PKSav

---
+++ testing/appveyor-scripts/build-test.bat
@@ -0,0 +1,29 @@
+@setlocal enableextensions enabledelayedexpansion
+@echo off
+
+mkdir c:\projects\libpkmn\test-env\build
+
+cd c:\projects\libpkmn\test-env\build
+if not !ERRORLEVEL!==0 goto fail
+
+cmake -G "%CMAKE_DASH_G%" -DCMAKE_BUILD_TYPE=Release ..\..
+if not !ERRORLEVEL!==0 goto fail
+
+msbuild /p:configuration=Release ALL_BUILD.vcxproj
+if not !ERRORLEVEL!==0 goto fail
+
+:: Don't run anything when cross-compiling
+if "x%CMAKE_DASH_G:ARM=%"=="x%CMAKE_DASH_G%" (
+    ctest --output-on-failure
+    if not !ERRORLEVEL!==0 goto fail
+)
+
+goto pass
+
+endlocal
+
+:fail
+exit /b 1
+
+:pass
+exit /b 0
