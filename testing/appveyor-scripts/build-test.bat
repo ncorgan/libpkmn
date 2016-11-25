3424339c9d6db053e70f08da82c80ee59bda93e8
Nicholas Corgan <n.corgan@gmail.com>
2016-11-25 11:15:34 -0600

    appveyor: trying other batch variable syntax

--- testing/appveyor-scripts/build-test.bat
+++ testing/appveyor-scripts/build-test.bat
@@ -6,7 +6,7 @@ mkdir c:\projects\libpkmn\test-env\build
 cd c:\projects\libpkmn\test-env\build
 if not !ERRORLEVEL!==0 goto fail
 
-%PYTHON_ROOT%\scripts\pip install ply CppHeaderParser
+"!PYTHON_ROOT!\scripts\pip" install ply CppHeaderParser
 if not !ERRORLEVEL!==0 goto fail
 
 cmake -G %CMAKE_GENERATOR_NAME% ^
