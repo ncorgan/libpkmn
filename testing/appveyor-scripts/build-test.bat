2a8c6a444e977c049fee8e98d5db1cf914c5949f
Nicholas Corgan <n.corgan@gmail.com>
2016-11-25 11:29:00 -0600

    appveyor: set paths in variables

--- testing/appveyor-scripts/build-test.bat
+++ testing/appveyor-scripts/build-test.bat
@@ -2,11 +2,15 @@
 @echo off
 
 :: Set variables
-if "%BITNESS%"=="32" set CMAKE_GENERATOR_NAME="Visual Studio 14 2015"
-if "%BITNESS%"=="64" set CMAKE_GENERATOR_NAME="Visual Studio 14 2015 Win64"
-if "%BITNESS%"=="32" set PYTHON_ROOT="C:\Python27"
-if "%BITNESS%"=="64" set PYTHON_ROOT="C:\Python27-x64"
-set BOOST_LIBRARY_DIR="%BOOST_ROOT%\lib%BITNESS%-msvc-14.0"
+if "%BITNESS%"=="32" (
+    set CMAKE_GENERATOR_NAME="Visual Studio 14 2015"
+    set PYTHON_ROOT=C:\Python27
+) else (
+    set CMAKE_GENERATOR_NAME="Visual Studio 14 2015 Win64"
+    set PYTHON_ROOT=C:\Python27-x64
+)
+set PYTHON_EXE="!PYTHON_ROOT!\python.exe"
+set PYTHON_PIP_EXE="!PYTHON_ROOT!\Scripts\pip.exe"
 
 mkdir c:\projects\libpkmn\test-env\build
 
@@ -14,15 +18,17 @@ cd c:\projects\libpkmn\test-env\build
 if not !ERRORLEVEL!==0 goto fail
 
 echo !PYTHON_ROOT!
-dir !PYTHON_ROOT!
-!PYTHON_ROOT!\scripts\pip install ply CppHeaderParser
+echo !PYTHON_EXE!
+echo !PYTHON_PIP_EXE!
+
+!PYTHON_PIP_EXE! install ply CppHeaderParser
 if not !ERRORLEVEL!==0 goto fail
 
 cmake -G %CMAKE_GENERATOR_NAME% ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DBOOST_ROOT="%BOOST_ROOT%" ^
     -DBoost_LIBRARY_DIRS=%BOOST_LIBRARY_DIR% ^
-    -DPYTHON_EXECUTABLE="%PYTHON_ROOT%\python.exe" ^
+    -DPYTHON_EXECUTABLE=!PYTHON_EXE! ^
     ..\..
 if not !ERRORLEVEL!==0 goto fail
 
