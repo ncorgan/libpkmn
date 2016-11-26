c0870f5c6d88caaa27d2b6bcec5ab9949edaebe7
Nicholas Corgan <n.corgan@gmail.com>
2016-11-25 18:14:25 -0600

    testing: ported non-pkmntest Python tests to Python unit test framework

--- testing/unit-tests/python_lists_test.py
+++ testing/unit-tests/python_lists_test.py
@@ -7,269 +7,105 @@
 #
 
 import pkmn
-import sys
-import traceback
 
-def ability_list_test():
-    try:
+import sys
+import unittest
+
+class lists_test(unittest.TestCase):
+
+    def test_ability_list(self):
         # Make sure trying to create an invalid list results in an error
-        try:
+        with self.assertRaises(IndexError):
             ability_list = pkmn.database.get_ability_list(0)
-        except IndexError:
-            pass
-        except:
-            raise RuntimeError("This should have thrown an IndexError.")
 
         ability_list = pkmn.database.get_ability_list(6)
-        if len(ability_list) != 191:
-            raise RuntimeError("len(ability_list) ({0}) != 191".format(len(ability_list)))
+        self.assertEqual(len(ability_list), 191)
+        self.assertEqual(ability_list[0], "Adaptability")
+        self.assertEqual(ability_list[-1], "Zen Mode")
 
-        if ability_list[0] != "Adaptability":
-            raise RuntimeError("ability_list[0] (\"{0}\") != \"Adaptability\"".format(ability_list[0]))
-        if ability_list[-1] != "Zen Mode":
-            raise RuntimeError("ability_list[-1] (\"{0}\") != \"Zen Mode\"".format(ability_list[-1]))
-
-        return True
-    except:
-        print()
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        traceback.print_exception(exc_type, exc_value, exc_traceback,
-                                  limit=5, file=sys.stdout)
-        return False
-
-def game_list_test():
-    try:
+    def test_game_list(self):
         # Make sure trying to create an invalid list results in an error
-        try:
+        with self.assertRaises(IndexError):
             game_list = pkmn.database.get_game_list(7, False)
-        except IndexError:
-            pass
-        except:
-            raise RuntimeError("This should have thrown an IndexError.")
 
         game_list = pkmn.database.get_game_list(6, True)
-        if len(game_list) != 26:
-            raise RuntimeError("len(game_list) ({0}) != 26".format(len(game_list)))
+        self.assertEqual(len(game_list), 26)
+        self.assertEqual(game_list[0], "Red")
+        self.assertEqual(game_list[-1], "Alpha Sapphire")
 
-        if game_list[0] != "Red":
-            raise RuntimeError("game_list[0] (\"{0}\") != \"Red\"".format(game_list[0]))
-        if game_list[-1] != "Alpha Sapphire":
-            raise RuntimeError("game_list[-1] (\"{0}\") != \"Alpha Sapphire\"".format(game_list[-1]))
-
-        return True
-    except:
-        print()
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        traceback.print_exception(exc_type, exc_value, exc_traceback,
-                                  limit=5, file=sys.stdout)
-        return False
-
-def item_list_test():
-    try:
+    def test_item_list(self):
         # Make sure trying to create an invalid list results in an error
-        try:
-            item_list = pkmn.database.get_item_list("Not a game")
-        except ValueError:
-            pass
-        except:
-            raise RuntimeError("This should have thrown a ValueError.")
+        with self.assertRaises(ValueError):
+            item_list = pkmn.database.get_item_list("not a game")
 
         item_list = pkmn.database.get_item_list("HeartGold")
-        if len(item_list) != 513:
-            raise RuntimeError("len(item_list) ({0}) != 513".format(len(item_list)))
+        self.assertEqual(len(item_list), 513)
+        self.assertEqual(item_list[0], "Adamant Orb")
+        self.assertEqual(item_list[-1], "Zoom Lens")
 
-        if item_list[0] != "Adamant Orb":
-            raise RuntimeError("item_list[0] (\"{0}\") != \"Adamant Orb\"".format(item_list[0]))
-        if item_list[-1] != "Zoom Lens":
-            raise RuntimeError("item_list[-1] (\"{0}\") != \"Zoom Lens\"".format(item_list[-1]))
-
-        return True
-    except:
-        print()
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        traceback.print_exception(exc_type, exc_value, exc_traceback,
-                                  limit=5, file=sys.stdout)
-        return False
-
-def location_list_test():
-    try:
+    def test_location_list(self):
         # Make sure trying to create an invalid list results in an error
-        try:
+        with self.assertRaises(ValueError):
             location_list = pkmn.database.get_location_list("Not a game", False)
-        except ValueError:
-            pass
-        except:
-            raise RuntimeError("This should have thrown a ValueError.")
 
         location_list = pkmn.database.get_location_list("Emerald", False)
-        if len(location_list) != 106:
-            raise RuntimeError("len(location_list) ({0}) != 106".format(len(location_list)))
+        self.assertEqual(len(location_list), 106)
+        self.assertEqual(location_list[0], "Abandoned Ship")
+        self.assertEqual(location_list[-1], "Victory Road")
 
-        if location_list[0] != "Abandoned Ship":
-            raise RuntimeError("location_list[0] (\"{0}\") != \"Abandoned Ship\"".format(location_list[0]))
-        if location_list[-1] != "Victory Road":
-            raise RuntimeError("location_list[-1] (\"{0}\") != \"Victory Road\"".format(location_list[-1]))
-
-        return True
-    except:
-        print()
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        traceback.print_exception(exc_type, exc_value, exc_traceback,
-                                  limit=5, file=sys.stdout)
-        return False
-
-def move_list_test():
-    try:
+    def test_move_list(self):
         # Make sure trying to create an invalid list results in an error
-        try:
+        with self.assertRaises(ValueError):
             move_list = pkmn.database.get_move_list("Not a game")
-        except ValueError:
-            pass
-        except:
-            raise RuntimeError("This should have thrown a ValueError.")
 
         move_list = pkmn.database.get_move_list("Red")
-        if len(move_list) != 165:
-            raise RuntimeError("len(move_list) ({0}) != 165".format(len(move_list)))
+        self.assertEqual(len(move_list), 165)
+        self.assertEqual(move_list[0], "Pound")
+        self.assertEqual(move_list[-1], "Struggle")
 
-        if move_list[0] != "Pound":
-            raise RuntimeError("move_list[0] (\"{0}\") != \"Pound\"".format(move_list[0]))
-        if move_list[-1] != "Struggle":
-            raise RuntimeError("move_list[-1] (\"{0}\") != \"Struggle\"".format(move_list[-1]))
-
-        return True
-    except:
-        print()
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        traceback.print_exception(exc_type, exc_value, exc_traceback,
-                                  limit=5, file=sys.stdout)
-        return False
-
-def nature_list_test():
-    try:
+    def test_nature_list(self):
         nature_list = pkmn.database.get_nature_list()
-        if len(nature_list) != 25:
-            raise RuntimeError("len(nature_list) ({0}) != 25".format(len(nature_list)))
+        self.assertEqual(len(nature_list), 25)
+        self.assertEqual(nature_list[0], "Adamant")
+        self.assertEqual(nature_list[-1], "Timid")
 
-        if nature_list[0] != "Adamant":
-            raise RuntimeError("nature_list[0] (\"{0}\") != \"Adamant\"".format(nature_list[0]))
-        if nature_list[-1] != "Timid":
-            raise RuntimeError("nature_list[-1] (\"{0}\") != \"Timid\"".format(nature_list[-1]))
-
-        return True
-    except:
-        print()
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        traceback.print_exception(exc_type, exc_value, exc_traceback,
-                                  limit=5, file=sys.stdout)
-        return False
-
-def pokemon_list_test():
-    try:
+    def test_pokemon_list(self):
         # Make sure trying to create an invalid list results in an error
-        try:
+        with self.assertRaises(IndexError):
             pokemon_list = pkmn.database.get_pokemon_list(0, True)
-        except IndexError:
-            pass
-        except:
-            raise RuntimeError("This should have thrown an IndexError.")
 
         pokemon_list = pkmn.database.get_pokemon_list(1, True)
-        if len(pokemon_list) != 151:
-            raise RuntimeError("len(pokemon_list) ({0}) != 151".format(len(pokemon_list)))
+        self.assertEqual(len(pokemon_list), 151)
+        self.assertEqual(pokemon_list[0], "Bulbasaur")
+        self.assertEqual(pokemon_list[-1], "Mew")
 
-        if pokemon_list[0] != "Bulbasaur":
-            raise RuntimeError("pokemon_list[0] (\"{0}\") != \"Bulbasaur\"".format(pokemon_list[0]))
-        if pokemon_list[-1] != "Mew":
-            raise RuntimeError("pokemon_list[-1] (\"{0}\") != \"Mew\"".format(pokemon_list[-1]))
-
-        return True
-    except:
-        print()
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        traceback.print_exception(exc_type, exc_value, exc_traceback,
-                                  limit=5, file=sys.stdout)
-        return False
-
-def region_list_test():
-    try:
+    def test_region_list(self):
         region_list = pkmn.database.get_region_list()
-        if len(region_list) != 7:
-            raise RuntimeError("len(region_list) ({0}) != 7".format(len(region_list)))
+        self.assertEqual(len(region_list), 7)
+        self.assertEqual(region_list[0], "Kanto")
+        self.assertEqual(region_list[-1], "Kalos")
 
-        if region_list[0] != "Kanto":
-            raise RuntimeError("region_list[0] (\"{0}\") != \"Kanto\"".format(region_list[0]))
-        if region_list[-1] != "Kalos":
-            raise RuntimeError("region_list[-1] (\"{0}\") != \"Kalos\"".format(region_list[-1]))
+    @unittest.skip("Function not yet implemented")
+    def test_ribbon_list(self):
+        ribbon_list = pkmn.database.get_ribbon_list(3)
 
-        return True
-    except:
-        print()
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        traceback.print_exception(exc_type, exc_value, exc_traceback,
-                                  limit=5, file=sys.stdout)
-        return False
-
-# TODO when database branch is merged in
-def ribbon_list_test():
-    ribbon_list = pkmn.database.get_ribbon_list(1)
-    return True
-
-def super_training_medal_list_test():
-    try:
+    def test_super_training_medal_list(self):
         super_training_medal_list = pkmn.database.get_super_training_medal_list()
-        if len(super_training_medal_list) != 30:
-            raise RuntimeError("len(super_training_medal_list) ({0}) != 30".format(len(super_training_medal_list)))
+        self.assertEqual(len(super_training_medal_list), 30)
+        self.assertEqual(super_training_medal_list[0], "Sp. Atk Level 1")
+        self.assertEqual(super_training_medal_list[-1], "The Battle for the Best!")
 
-        if super_training_medal_list[0] != "Sp. Atk Level 1":
-            raise RuntimeError("super_training_medal_list[0] (\"{0}\") != \"Sp. Atk Level 1\"".format(super_training_medal_list[0]))
-        if super_training_medal_list[-1] != "The Battle for the Best!":
-            raise RuntimeError("super_training_medal_list[-1] (\"{0}\") != \"The Battle for the Best!\"".format(super_training_medal_list[-1]))
-
-        return True
-    except:
-        print()
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        traceback.print_exception(exc_type, exc_value, exc_traceback,
-                                  limit=5, file=sys.stdout)
-        return False
-
-def type_list_test():
-    try:
+    def test_type_list(self):
         # Make sure trying to create an invalid list results in an error
-        try:
+        with self.assertRaises(ValueError):
             type_list = pkmn.database.get_type_list("Not a game")
-        except ValueError:
-            pass
-        except:
-            raise RuntimeError("This should have thrown a ValueError.")
 
         type_list = pkmn.database.get_type_list("Alpha Sapphire")
-        if len(type_list) != 18:
-            raise RuntimeError("len(type_list) ({0}) != 18".format(len(type_list)))
-
-        if type_list[0] != "Normal":
-            raise RuntimeError("type_list[0] (\"{0}\") != \"Normal\"".format(type_list[0]))
-        if type_list[-1] != "Fairy":
-            raise RuntimeError("type_list[-1] (\"{0}\") != \"Fairy\"".format(type_list[-1]))
-
-        return True
-    except:
-        print()
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        traceback.print_exception(exc_type, exc_value, exc_traceback,
-                                  limit=5, file=sys.stdout)
-        return False
+        self.assertEqual(len(type_list), 18)
+        self.assertEqual(type_list[0], "Normal")
+        self.assertEqual(type_list[-1], "Fairy")
 
 if __name__ == "__main__":
-
-    successful = True
-    for test in [ability_list_test, game_list_test, item_list_test, location_list_test, \
-                 move_list_test, nature_list_test, pokemon_list_test, region_list_test, \
-                 ribbon_list_test, super_training_medal_list_test, type_list_test]:
-        sys.stdout.write("Starting test %s..." % test.__name__)
-        result = test()
-        print("success.\n" if result else "failed.\n")
-        successful = successful and result
-
-    sys.exit(0 if successful else 1)
+    suite = unittest.TestLoader().loadTestsFromTestCase(lists_test)
+    test_program = unittest.TextTestRunner(verbosity=3).run(suite)
+    sys.exit(0 if test_program.wasSuccessful() else 1)
--- testing/unit-tests/python_paths_test.py
+++ testing/unit-tests/python_paths_test.py
@@ -1,39 +1,30 @@
 #!/usr/bin/env python
 #
-# Copyright (c) 2015-2016 Nicholas Corgan (n.corgan@gmail.com)
+# Copyright (c) 2016 Nicholas Corgan (n.corgan@gmail.com)
 #
 # Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
 # or copy at http://opensource.org/licenses/MIT)
 #
 
+import pkmn
+
 import os
 import platform
-import pkmn
 import sys
-import traceback
+import unittest
 
-def appdata_dir_test():
-    try:
+class paths_test(unittest.TestCase):
+
+    def test_appdata_dir(self):
         if platform.system() == "Windows":
             appdata_dir = "C:\\libpkmn\\appdata\\dir"
         else:
             appdata_dir = "/libpkmn/appdata/dir"
 
         os.putenv("PKMN_APPDATA_DIR", appdata_dir)
-        pkmn_appdata_dir = pkmn.get_appdata_dir()
-        if pkmn_appdata_dir != appdata_dir:
-            raise RuntimeError("pkmn_appdata_dir (\"{0}\") != \"{1}\"".format(pkmn_appdata_dir, appdata_dir))
+        self.assertEqual(pkmn.get_appdata_dir(), appdata_dir)
 
-        return True
-    except:
-        print()
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        traceback.print_exception(exc_type, exc_value, exc_traceback,
-                                  limit=5, file=sys.stdout)
-        return False
-
-def database_path_test():
-    try:
+    def test_database_path(self):
         #
         # When this unit test is run, the PKMN_DATABASE_PATH environment variable
         # is set to a valid value, so just make sure the call doesn't fail.
@@ -41,74 +32,33 @@ def database_path_test():
         pkmn.get_database_path()
 
         # Cause a failure with an existing file that isn't a database
-        os.putenv("PKMN_DATABASE_PATH", os.path.realpath(__file__))
-        try:
+        with self.assertRaises(RuntimeError):
+            os.putenv("PKMN_DATABASE_PATH", os.path.realpath(__file__))
             pkmn.get_database_path()
-        except RuntimeError:
-            pass
-        except:
-            raise RuntimeError("The path \"{0}\" should not be considered a valid database.".format(os.path.realpath(__file__)))
 
         # Cause a failure with a non-existent file
         if platform.system() == "Windows":
             database_path = "C:\\libpkmn\\database\\path"
         else:
             database_path = "/libpkmn/database/path"
-
-        os.putenv("PKMN_DATABASE_PATH", database_path)
-        try:
+        with self.assertRaises(RuntimeError):
+            os.putenv("PKMN_DATABASE_PATH", database_path)
             pkmn.get_database_path()
-        except RuntimeError:
-            pass
-        except:
-            raise RuntimeError("The path \"{0}\" should not be considered a valid database.".format(database_path))
 
-        return True
-    except:
-        print()
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        traceback.print_exception(exc_type, exc_value, exc_traceback,
-                                  limit=5, file=sys.stdout)
-        return False
-
-def images_dir_test():
-    try:
+    def test_images_dir(self):
         if platform.system() == "Windows":
             images_dir = "C:\\libpkmn\\images\\dir"
         else:
             images_dir = "/libpkmn/images/dir"
 
         os.putenv("PKMN_IMAGES_DIR", images_dir)
-        pkmn_images_dir = pkmn.get_images_dir()
-        if pkmn_images_dir != images_dir:
-            raise RuntimeError("pkmn_images_dir (\"{0}\") != \"{1}\"".format(pkmn_images_dir, images_dir))
+        self.assertEqual(pkmn.get_images_dir(), images_dir)
 
-        return True
-    except:
-        print()
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        traceback.print_exception(exc_type, exc_value, exc_traceback,
-                                  limit=5, file=sys.stdout)
-        return False
-
-# TODO
-def tmp_dir_test():
-    try:
-        return True
-    except:
-        print()
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        traceback.print_exception(exc_type, exc_value, exc_traceback,
-                                  limit=5, file=sys.stdout)
-        return False
+    @unittest.skip("TODO")
+    def test_tmp_dir(self):
+        pkmn.get_tmp_dir()
 
 if __name__ == "__main__":
-
-    successful = True
-    for test in [appdata_dir_test, database_path_test, images_dir_test, tmp_dir_test]:
-        sys.stdout.write("Starting test %s..." % test.__name__)
-        result = test()
-        print("success.\n" if result else "failed.\n")
-        successful = successful and result
-
-    sys.exit(0 if successful else 1)
+    suite = unittest.TestLoader().loadTestsFromTestCase(paths_test)
+    test_program = unittest.TextTestRunner(verbosity=3).run(suite)
+    sys.exit(0 if test_program.wasSuccessful() else 1)
