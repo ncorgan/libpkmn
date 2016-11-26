2ec0e5caa8ba4e398f0c06e267af4f8378fea0bb
Nicholas Corgan <n.corgan@gmail.com>
2016-11-26 10:07:14 -0600

    cmake: find Qt5, added Qt5 Widgets component

---
+++ cmake/Modules/PKMNQt5.cmake
@@ -0,0 +1,11 @@
+#
+# Copyright (c) 2016 Nicholas Corgan (n.corgan@gmail.com)
+#
+# Distributed under the MIT License (MIT) (See accompanying FILE LICENSE.txt
+# or copy at http://opensource.org/licenses/MIT)
+#
+
+SET(CMAKE_AUTOMOC ON)
+SET(CMAKE_INCLUDE_CURRENT_DIR ON)
+
+FIND_PACKAGE(Qt5Widgets)
