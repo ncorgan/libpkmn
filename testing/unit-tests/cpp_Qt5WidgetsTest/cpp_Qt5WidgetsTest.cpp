2d13c82458dc69f3793532a1cbd6ebe70daac31f
Nicholas Corgan <n.corgan@gmail.com>
2016-11-26 21:03:52 -0600

    Starting unit test

---
+++ testing/unit-tests/cpp_Qt5WidgetsTest/cpp_Qt5WidgetsTest.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2016 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+
+#include "cpp_Qt5WidgetsTest.hpp"
+
+#include <pkmn/qt5/AbilityListComboBox.hpp>
+
+void Qt5WidgetsTest::testAbilityListComboBox() {
+}
+
+QTEST_MAIN(Qt5WidgetsTest)
+#include "moc_cpp_Qt5WidgetsTest.cpp"
---
+++ testing/unit-tests/cpp_Qt5WidgetsTest/cpp_Qt5WidgetsTest.hpp
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2016 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+
+#ifndef CPP_QT5WIDGETSTEST_HPP
+#define CPP_QT5WIDGETSTEST_HPP
+
+#include <QtTest/QtTest>
+
+class Qt5WidgetsTest: public QObject
+{
+    Q_OBJECT
+
+    private slots:
+        void testAbilityListComboBox();
+};
+
+#endif /* CPP_QT5WIDGETSTEST_HPP */
