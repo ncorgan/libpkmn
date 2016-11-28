7548e22aef6934c734b922ddbc633931fbf76d51
Nicholas Corgan <n.corgan@gmail.com>
2016-11-28 06:12:07 -0600

    pkmntest: fully implemented new function with 8 items

--- testing/unit-tests/pkmntest/cpp/items_tests_common.cpp
+++ testing/unit-tests/pkmntest/cpp/items_tests_common.cpp
@@ -44,6 +44,7 @@ namespace pkmntest {
             list->remove(item_name, 100)
         , std::out_of_range);
 
+        // This shouldn't have added any items
         BOOST_CHECK_EQUAL(list->get_num_items(), num_items);
     }
 
@@ -51,8 +52,16 @@ namespace pkmntest {
         pkmn::item_list::sptr list,
         const std::vector<std::string> &item_names
     ) {
-        (void)list;
-        (void)item_names;
+        int num_items = list->get_num_items();
+
+        for(auto iter = item_names.begin(); iter != item_names.end(); ++iter) {
+            BOOST_CHECK_THROW(
+                list->add(*iter, 1)
+            , std::invalid_argument);
+        }
+
+        // This shouldn't have added any items
+        BOOST_CHECK_EQUAL(list->get_num_items(), num_items);
     }
 
     void test_item_list_add_remove(
@@ -110,5 +119,97 @@ namespace pkmntest {
         BOOST_CHECK(item_slots.at(2).item == none_entry);
         BOOST_CHECK_EQUAL(item_slots.at(2).amount, 0);
         BOOST_CHECK_EQUAL(list->get_num_items(), 2);
+
+        list->add(item_names[3], 90);
+        BOOST_CHECK_EQUAL(item_slots.at(0).item.get_name(), item_names[1]);
+        BOOST_CHECK_EQUAL(item_slots.at(0).amount, 79);
+        BOOST_CHECK_EQUAL(item_slots.at(1).item.get_name(), item_names[2]);
+        BOOST_CHECK_EQUAL(item_slots.at(1).amount, 16);
+        BOOST_CHECK_EQUAL(item_slots.at(2).item.get_name(), item_names[3]);
+        BOOST_CHECK_EQUAL(item_slots.at(2).amount, 90);
+        BOOST_CHECK_EQUAL(list->get_num_items(), 3);
+
+        list->add(item_names[4], 2);
+        BOOST_CHECK_EQUAL(item_slots.at(0).item.get_name(), item_names[1]);
+        BOOST_CHECK_EQUAL(item_slots.at(0).amount, 79);
+        BOOST_CHECK_EQUAL(item_slots.at(1).item.get_name(), item_names[2]);
+        BOOST_CHECK_EQUAL(item_slots.at(1).amount, 16);
+        BOOST_CHECK_EQUAL(item_slots.at(2).item.get_name(), item_names[3]);
+        BOOST_CHECK_EQUAL(item_slots.at(2).amount, 90);
+        BOOST_CHECK_EQUAL(item_slots.at(3).item.get_name(), item_names[4]);
+        BOOST_CHECK_EQUAL(item_slots.at(3).amount, 2);
+        BOOST_CHECK_EQUAL(list->get_num_items(), 4);
+
+        list->remove(item_names[1], 30);
+        BOOST_CHECK_EQUAL(item_slots.at(0).item.get_name(), item_names[1]);
+        BOOST_CHECK_EQUAL(item_slots.at(0).amount, 49);
+        BOOST_CHECK_EQUAL(item_slots.at(1).item.get_name(), item_names[2]);
+        BOOST_CHECK_EQUAL(item_slots.at(1).amount, 16);
+        BOOST_CHECK_EQUAL(item_slots.at(2).item.get_name(), item_names[3]);
+        BOOST_CHECK_EQUAL(item_slots.at(2).amount, 90);
+        BOOST_CHECK_EQUAL(item_slots.at(3).item.get_name(), item_names[4]);
+        BOOST_CHECK_EQUAL(item_slots.at(3).amount, 2);
+        BOOST_CHECK_EQUAL(list->get_num_items(), 4);
+
+        list->add(item_names[5], 12);
+        BOOST_CHECK_EQUAL(item_slots.at(0).item.get_name(), item_names[1]);
+        BOOST_CHECK_EQUAL(item_slots.at(0).amount, 49);
+        BOOST_CHECK_EQUAL(item_slots.at(1).item.get_name(), item_names[2]);
+        BOOST_CHECK_EQUAL(item_slots.at(1).amount, 16);
+        BOOST_CHECK_EQUAL(item_slots.at(2).item.get_name(), item_names[3]);
+        BOOST_CHECK_EQUAL(item_slots.at(2).amount, 90);
+        BOOST_CHECK_EQUAL(item_slots.at(3).item.get_name(), item_names[4]);
+        BOOST_CHECK_EQUAL(item_slots.at(3).amount, 2);
+        BOOST_CHECK_EQUAL(item_slots.at(4).item.get_name(), item_names[5]);
+        BOOST_CHECK_EQUAL(item_slots.at(4).amount, 12);
+        BOOST_CHECK_EQUAL(list->get_num_items(), 5);
+
+        list->remove(item_names[2], 16);
+        BOOST_CHECK_EQUAL(item_slots.at(0).item.get_name(), item_names[1]);
+        BOOST_CHECK_EQUAL(item_slots.at(0).amount, 49);
+        BOOST_CHECK_EQUAL(item_slots.at(1).item.get_name(), item_names[3]);
+        BOOST_CHECK_EQUAL(item_slots.at(1).amount, 90);
+        BOOST_CHECK_EQUAL(item_slots.at(2).item.get_name(), item_names[4]);
+        BOOST_CHECK_EQUAL(item_slots.at(2).amount, 2);
+        BOOST_CHECK_EQUAL(item_slots.at(3).item.get_name(), item_names[5]);
+        BOOST_CHECK_EQUAL(item_slots.at(3).amount, 12);
+        BOOST_CHECK_EQUAL(list->get_num_items(), 4);
+
+        list->add(item_names[6], 65);
+        BOOST_CHECK_EQUAL(item_slots.at(0).item.get_name(), item_names[1]);
+        BOOST_CHECK_EQUAL(item_slots.at(0).amount, 49);
+        BOOST_CHECK_EQUAL(item_slots.at(1).item.get_name(), item_names[3]);
+        BOOST_CHECK_EQUAL(item_slots.at(1).amount, 90);
+        BOOST_CHECK_EQUAL(item_slots.at(2).item.get_name(), item_names[4]);
+        BOOST_CHECK_EQUAL(item_slots.at(2).amount, 2);
+        BOOST_CHECK_EQUAL(item_slots.at(3).item.get_name(), item_names[5]);
+        BOOST_CHECK_EQUAL(item_slots.at(3).amount, 12);
+        BOOST_CHECK_EQUAL(item_slots.at(4).item.get_name(), item_names[6]);
+        BOOST_CHECK_EQUAL(item_slots.at(4).amount, 65);
+        BOOST_CHECK_EQUAL(list->get_num_items(), 5);
+
+        list->add(item_names[7], 6);
+        BOOST_CHECK_EQUAL(item_slots.at(0).item.get_name(), item_names[1]);
+        BOOST_CHECK_EQUAL(item_slots.at(0).amount, 49);
+        BOOST_CHECK_EQUAL(item_slots.at(1).item.get_name(), item_names[3]);
+        BOOST_CHECK_EQUAL(item_slots.at(1).amount, 90);
+        BOOST_CHECK_EQUAL(item_slots.at(2).item.get_name(), item_names[4]);
+        BOOST_CHECK_EQUAL(item_slots.at(2).amount, 2);
+        BOOST_CHECK_EQUAL(item_slots.at(3).item.get_name(), item_names[5]);
+        BOOST_CHECK_EQUAL(item_slots.at(3).amount, 12);
+        BOOST_CHECK_EQUAL(item_slots.at(4).item.get_name(), item_names[6]);
+        BOOST_CHECK_EQUAL(item_slots.at(4).amount, 65);
+        BOOST_CHECK_EQUAL(item_slots.at(5).item.get_name(), item_names[7]);
+        BOOST_CHECK_EQUAL(item_slots.at(5).amount, 6);
+        BOOST_CHECK_EQUAL(list->get_num_items(), 6);
+
+        // Remove everything
+        list->remove(item_names[4], 2);
+        list->remove(item_names[1], 49);
+        list->remove(item_names[7], 6);
+        list->remove(item_names[5], 12);
+        list->remove(item_names[3], 90);
+        list->remove(item_names[6], 65);
+        BOOST_CHECK_EQUAL(list->get_num_items(), 0);
     }
 }
