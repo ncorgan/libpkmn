a10ac7cc89f0f73b8fa60b92c5b9965c3a535cb6
Nicholas Corgan <n.corgan@gmail.com>
2016-12-02 18:54:39 -0600

    travis: installing Qt

--- testing/travis-scripts/install-deps.sh
+++ testing/travis-scripts/install-deps.sh
@@ -22,7 +22,7 @@ else
     [ $? -ne 0 ] && exit 1
     sudo apt-get -y install build-essential cmake clang cppcheck valgrind \
 	                    doxygen libpython-dev python-ply libboost1.55-all-dev \
-			    swig python-pip libqt4-dev qt5base-dev
+			    swig python-pip libqt4-dev qtbase5-dev
     [ $? -ne 0 ] && exit 1
     sudo pip install CppHeaderParser
     [ $? -ne 0 ] && exit 1
