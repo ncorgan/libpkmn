2172adac403a2f95922620bc596daaafe018a912
Nicholas Corgan <n.corgan@gmail.com>
2016-12-11 15:56:00 -0600

    Added scoped_lock

---
+++ lib/mem/scoped_lock.hpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2016 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+
+#ifndef INCLUDED_PKMN_MEM_SCOPED_LOCK_HPP
+#define INCLUDED_PKMN_MEM_SCOPED_LOCK_HPP
+
+#include <pkmn/config.hpp>
+
+#include <boost/thread/mutex.hpp>
+
+namespace pkmn { namespace mem {
+
+    template <typename T>
+    class scoped_lock {
+        public:
+            PKMN_INLINE scoped_lock(
+                T* impl
+            ): _impl(impl)
+            {
+                _impl->_mem_mutex.lock();
+            }
+
+            PKMN_INLINE ~scoped_lock() {
+                _impl->_mem_mutex.unlock();
+            }
+
+        private:
+            T* _impl;
+    };
+
+}}
+
+#endif /* INCLUDED_PKMN_MEM_SCOPED_LOCK_HPP */
