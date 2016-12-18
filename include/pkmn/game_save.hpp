2cd80ca48f2b2608736d9f4ecb0716390ac9429d
Nicholas Corgan <n.corgan@gmail.com>
2016-12-18 13:30:32 -0600

    Added game_save header with getter functions

---
+++ include/pkmn/game_save.hpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2016 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+#ifndef PKMN_ITEM_BAG_HPP
+#define PKMN_ITEM_BAG_HPP
+
+#include <pkmn/config.hpp>
+#include <pkmn/item_bag.hpp>
+#include <pkmn/item_list.hpp>
+#include <pkmn/types/shared_ptr.hpp>
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+namespace pkmn {
+
+    class PKMN_API game_save {
+        public:
+            typedef pkmn::shared_ptr<game_save> sptr;
+
+            #ifndef __DOXYGEN__
+            game_save() {};
+            virtual ~game_save() {};
+            #endif
+
+            virtual void load() = 0;
+
+            virtual std::string get_filepath() = 0;
+
+            virtual std::string get_game() = 0;
+
+            virtual std::string get_trainer_name() = 0;
+
+            virtual uint32_t get_trainer_id() = 0;
+
+            virtual uint16_t get_trainer_public_id() = 0;
+
+            virtual uint16_t get_trainer_secret_id() = 0;
+
+            virtual std::string get_trainer_gender() = 0;
+
+            virtual std::string get_rival_name() = 0;
+
+            virtual int get_money() = 0;
+
+            virtual pkmn::item_bag::sptr get_item_bag() = 0;
+
+            virtual pkmn::item_list::sptr get_item_pc() = 0;
+    };
+}
