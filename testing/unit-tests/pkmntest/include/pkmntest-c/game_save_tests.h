23de078753c398f638d0b5484b619b0510b50d1b
Nicholas Corgan <n.corgan@gmail.com>
2017-02-02 21:06:05 -0600

    pkmntest: PKMN_API -> PKMNTEST_API

--- testing/unit-tests/pkmntest/include/pkmntest-c/game_save_tests.h
+++ testing/unit-tests/pkmntest/include/pkmntest-c/game_save_tests.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Nicholas Corgan (n.corgan@gmail.com)
+ * Copyright (c) 2016-2017 Nicholas Corgan (n.corgan@gmail.com)
  *
  * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
  * or copy at http://opensource.org/licenses/MIT)
@@ -8,13 +8,15 @@
 #ifndef GBA_GAME_SAVE_TESTS_H
 #define GBA_GAME_SAVE_TESTS_H
 
+#include <pkmntest-c/config.h>
+
 #include <pkmn-c/game_save.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-PKMN_API void pkmntest_game_save_test_common_fields(
+PKMNTEST_API void pkmntest_game_save_test_common_fields(
     pkmn_game_save_handle_t save
 );
 
