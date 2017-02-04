b88ccdafd6c7c6253d2e98baca053c5b01a30be2
Nicholas Corgan <n.corgan@gmail.com>
2017-02-04 10:51:19 -0600

    Bumped database compat num to 6

--- lib/database/database_common.cpp
+++ lib/database/database_common.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Nicholas Corgan (n.corgan@gmail.com)
+ * Copyright (c) 2017 Nicholas Corgan (n.corgan@gmail.com)
  *
  * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
  * or copy at http://opensource.org/licenses/MIT)
@@ -20,7 +20,7 @@
 #include <stdexcept>
 #include <string>
 
-#define PKMN_COMPAT_NUM 5
+#define PKMN_COMPAT_NUM 6
 
 namespace pkmn { namespace database {
 
