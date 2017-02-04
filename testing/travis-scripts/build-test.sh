511a1d975646f7ad0394419d8a817a4d4acf86e3
Nicholas Corgan <n.corgan@gmail.com>
2017-02-04 15:09:21 -0600

    travis: fix Maven POM directory, temporarily disable non-Java builds

--- testing/travis-scripts/build-test.sh
+++ testing/travis-scripts/build-test.sh
@@ -26,42 +26,46 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     [ $? -ne 0 ] && exit 1
 else
     # Check source
-    find $REPO_TOPLEVEL/lib $REPO_TOPLEVEL/include $REPO_TOPLEVEL/testing -name '*.[ch]pp' | xargs cppcheck --error-exitcode=1 --force 1>/dev/null
-    find $REPO_TOPLEVEL/lib $REPO_TOPLEVEL/include $REPO_TOPLEVEL/testing -name '*.[ch]' | xargs cppcheck --error-exitcode=1 --force 1>/dev/null
-    [ $? -ne 0 ] && exit 1
-
-    mkdir -p gcc clang
-    export PYTHONPATH=/usr/lib/python2.7/dist-packages:/usr/lib/python2.7/site-packages:$PYTHONPATH
-    export PYTHONPATH=/usr/local/lib/python2.7/dist-packages:/usr/local/lib/python2.7/site-packages:$PYTHONPATH
-
-    # GCC compile check
-    cd $REPO_TOPLEVEL/test-env/gcc
-    [ $? -ne 0 ] && exit 1
-    cmake -DCMAKE_BUILD_TYPE=Release \
-	  -DDESIRED_QT_VERSION=$DESIRED_QT_VERSION \
-	  $REPO_TOPLEVEL
-    [ $? -ne 0 ] && exit 1
-    make
-    [ $? -ne 0 ] && exit 1
-    ctest -E ".*GUI" --output-on-failure
-    [ $? -ne 0 ] && exit 1
-
-    # Clang compile check
-    cd $REPO_TOPLEVEL/test-env/clang
-    [ $? -ne 0 ] && exit 1
-    CC=clang CXX=clang++ cmake -DCMAKE_BUILD_TYPE=Release \
-	                       -DDESIRED_QT_VERSION=$DESIRED_QT_VERSION \
-                               $REPO_TOPLEVEL
-    [ $? -ne 0 ] && exit 1
-    make
-    [ $? -ne 0 ] && exit 1
-    ctest -E ".*GUI" --output-on-failure
-    [ $? -ne 0 ] && exit 1
+#    find $REPO_TOPLEVEL/lib $REPO_TOPLEVEL/include $REPO_TOPLEVEL/testing -name '*.[ch]pp' | xargs cppcheck --error-exitcode=1 --force 1>/dev/null
+#    find $REPO_TOPLEVEL/lib $REPO_TOPLEVEL/include $REPO_TOPLEVEL/testing -name '*.[ch]' | xargs cppcheck --error-exitcode=1 --force 1>/dev/null
+#    [ $? -ne 0 ] && exit 1
+#
+#    mkdir -p gcc clang
+#    export PYTHONPATH=/usr/lib/python2.7/dist-packages:/usr/lib/python2.7/site-packages:$PYTHONPATH
+#    export PYTHONPATH=/usr/local/lib/python2.7/dist-packages:/usr/local/lib/python2.7/site-packages:$PYTHONPATH
+#
+#    # GCC compile check
+#    cd $REPO_TOPLEVEL/test-env/gcc
+#    [ $? -ne 0 ] && exit 1
+#    cmake -DCMAKE_BUILD_TYPE=Release \
+#	  -DDESIRED_QT_VERSION=$DESIRED_QT_VERSION \
+#	  $REPO_TOPLEVEL
+#    [ $? -ne 0 ] && exit 1
+#    make
+#    [ $? -ne 0 ] && exit 1
+#    ctest -E ".*GUI" --output-on-failure
+#    [ $? -ne 0 ] && exit 1
+#
+#    # Clang compile check
+#    cd $REPO_TOPLEVEL/test-env/clang
+#    [ $? -ne 0 ] && exit 1
+#    CC=clang CXX=clang++ cmake -DCMAKE_BUILD_TYPE=Release \
+#	                       -DDESIRED_QT_VERSION=$DESIRED_QT_VERSION \
+#                               $REPO_TOPLEVEL
+#    [ $? -ne 0 ] && exit 1
+#    make
+#    [ $? -ne 0 ] && exit 1
+#    ctest -E ".*GUI" --output-on-failure
+#    [ $? -ne 0 ] && exit 1
 
     # Build with Java
-    cd $REPO_TOPLEVEL/maven
+    cd $REPO_TOPLEVEL/maven/PKMN
     mvn test
+    [ $? -ne 0 ] && exit 1
     mvn site
+    [ $? -ne 0 ] && exit 1
+    mvn package
+    [ $? -ne 0 ] && exit 1
 fi
 
 echo # So we can check the last error code
