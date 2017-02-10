81a9958c0d1004174f4df46c76ec3134579dc84a
Nicholas Corgan <n.corgan@gmail.com>
2017-02-09 20:48:58 -0600

    java: added paths test that will go first

---
+++ maven/PKMN/src/test/java/nc/PKMN/AAAAA_PathsTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2017 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+
+package nc.PKMN;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AAAAA_PathsTest extends TestCase {
+    protected void setUp() {
+        try {
+            InitPKMN.initialize();
+        } catch(Exception e) {}
+    }
+
+    public AAAAA_PathsTest(String testName) {
+        super(testName);
+    }
+
+    public static Test suite() {
+        return new TestSuite(AAAAA_PathsTest.class);
+    }
+
+    public void testPaths() {
+        System.out.println(" * Appdata directory: " + nc.PKMN.Paths.getAppdataDir());
+        System.out.println(" * Database path:     " + nc.PKMN.Paths.getDatabasePath());
+        System.out.println(" * Images directory:  " + nc.PKMN.Paths.getImagesDir());
+        System.out.println(" * Temp directory:    " + nc.PKMN.Paths.getTmpDir());
+    }
+}
