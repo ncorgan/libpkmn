2953379fe684b709a67e830bdf28321311ee8fca
Nicholas Corgan <n.corgan@gmail.com>
2017-02-11 08:54:40 -0600

    travis: fix apt-get command

--- testing/travis-scripts/install-deps.sh
+++ testing/travis-scripts/install-deps.sh
@@ -10,11 +10,12 @@ REPO_TOPLEVEL=$PWD
 
 sudo apt-get -y update
 [ $? -ne 0 ] && exit 1
-sudo apt-get -y install build-essential cmake clang cppcheck valgrind \
-		    doxygen libpython-dev python-ply libboost1.55-all-dev \
-		    swig python-pip libqt4-dev qtbase5-dev mono-complete \
-		    swig3.0 python-pip libqt4-dev qtbase5-dev liblua5.1-dev \
-		    luarocks mono-complete monodevelop-nunit nunit-console
+sudo apt-get -y install cmake cppcheck libboost1.55-all-dev libqt4-dev qtbase5-dev \
+                        libgtest-dev swig swig3.0 doxygen \
+			mono-complete monodevelop-unit nunit-console \
+			default-jdk junit maven \
+			liblua5.1-dev luarocks \
+			python3 python-pip python3-pip
 [ $? -ne 0 ] && exit 1
 sudo pip install CppHeaderParser
 [ $? -ne 0 ] && exit 1
