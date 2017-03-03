2f16d66892f96df1ea2f34cfb87fc2db688b4f19
Nicholas Corgan <n.corgan@gmail.com>
2017-03-02 19:53:04 -0600

    cpp_game_save_test: more save comparison

--- testing/unit-tests/pkmntest/cpp/game_save_test.cpp
+++ testing/unit-tests/pkmntest/cpp/game_save_test.cpp
@@ -11,6 +11,10 @@
 
 #include <pkmn/exception.hpp>
 
+#include <pksav/gen1/pokemon.h>
+#include <pksav/gen2/pokemon.h>
+#include <pksav/gba/pokemon.h>
+
 #include <boost/assign.hpp>
 #include <boost/filesystem.hpp>
 
@@ -166,6 +170,7 @@ namespace pkmntest {
         pkmn::game_save::sptr save
     ) {
         std::string game = save->get_game();
+        int generation = game_generations.at(game);
 
         test_trainer_name(save);
 
@@ -231,6 +236,9 @@ namespace pkmntest {
         pkmn::pokemon_pc::sptr pc = save->get_pokemon_pc();
         int num_boxes = pc->get_num_boxes();
         EXPECT_EQ(pc->as_vector().size(), num_boxes);
+        if(generation >= 2) {
+            EXPECT_EQ(pc->as_vector().size(), pc->get_box_names().size());
+        }
         for(int i = 0; i < num_boxes; ++i) {
             pkmn::pokemon_box::sptr box = pc->get_box(i);
             ASSERT_NE(nullptr, box.get());
@@ -258,12 +266,75 @@ namespace pkmntest {
         }
     }
 
+    // TODO: specific case for Gamecube games
+    BOOST_STATIC_CONSTEXPR size_t pksav_pc_pokemon_sizes[] = {
+        0,
+        sizeof(pksav_gen1_pc_pokemon_t),
+        sizeof(pksav_gen2_pc_pokemon_t),
+        sizeof(pksav_gba_pc_pokemon_t)
+    };
+    BOOST_STATIC_CONSTEXPR size_t pksav_pokemon_party_data_sizes[] = {
+        0,
+        sizeof(pksav_gen1_pokemon_party_data_t),
+        sizeof(pksav_gen2_pokemon_party_data_t),
+        sizeof(pksav_gba_pokemon_party_data_t)
+    };
+
+    static void compare_item_lists(
+        pkmn::item_list::sptr list1,
+        pkmn::item_list::sptr list2
+    ) {
+        EXPECT_EQ(list1->get_name(), list2->get_name());
+        EXPECT_EQ(list1->get_capacity(), list2->get_capacity());
+        EXPECT_EQ(list1->get_num_items(), list2->get_num_items());
+
+        const pkmn::item_slots_t& item_slots1 = list1->as_vector();
+        const pkmn::item_slots_t& item_slots2 = list2->as_vector();
+        ASSERT_EQ(item_slots1.size(), item_slots2.size());
+        for(size_t i = 0; i < item_slots1.size(); ++i) {
+            EXPECT_EQ(item_slots1[i].item.get_item_id(), item_slots2[i].item.get_item_id());
+            EXPECT_EQ(item_slots1[i].amount, item_slots2[i].amount);
+        }
+    }
+
+    static void compare_pokemon(
+        pkmn::pokemon::sptr pokemon1,
+        pkmn::pokemon::sptr pokemon2
+    ) {
+        int generation = game_generations.at(pokemon1->get_game());
+
+        // Names are stored separately in Generations I-II.
+        if(generation < 3) {
+            EXPECT_EQ(pokemon1->get_nickname(), pokemon2->get_nickname());
+            EXPECT_EQ(pokemon1->get_trainer_name(), pokemon2->get_trainer_name());
+        }
+
+        // On the C++ level, check the underlying memory.
+        EXPECT_EQ(
+            0,
+            memcmp(
+                pokemon1->get_native_pc_data(),
+                pokemon2->get_native_pc_data(),
+                pksav_pc_pokemon_sizes[generation]
+            )
+        );
+        EXPECT_EQ(
+            0,
+            memcmp(
+                pokemon1->get_native_party_data(),
+                pokemon2->get_native_party_data(),
+                pksav_pokemon_party_data_sizes[generation]
+            )
+        );
+    }
+
     void check_two_game_saves_equal(
         pkmn::game_save::sptr save1,
         pkmn::game_save::sptr save2
     ) {
         ASSERT_EQ(save1->get_game(), save2->get_game());
         std::string game = save1->get_game();
+        int generation = game_generations.at(game);
 
         EXPECT_EQ(save1->get_trainer_name(), save2->get_trainer_name());
         EXPECT_EQ(save1->get_trainer_id(), save2->get_trainer_id());
@@ -281,13 +352,69 @@ namespace pkmntest {
 
         EXPECT_EQ(save1->get_money(), save2->get_money());
 
+        pkmn::item_bag::sptr item_bag1 = save1->get_item_bag();
+        pkmn::item_bag::sptr item_bag2 = save2->get_item_bag();
+
+        const pkmn::item_pockets_t& pockets1 = item_bag1->get_pockets();
+        const pkmn::item_pockets_t& pockets2 = item_bag2->get_pockets();
+        ASSERT_EQ(pockets1.size(), pockets2.size());
+
+        const std::vector<std::string>& pocket_names1 = item_bag1->get_pocket_names();
+        const std::vector<std::string>& pocket_names2 = item_bag2->get_pocket_names();
+        ASSERT_EQ(pocket_names1.size(), pocket_names2.size());
+        for(auto iter = pocket_names1.begin(); iter != pocket_names1.end(); ++iter) {
+            compare_item_lists(
+                pockets1.at(*iter),
+                pockets2.at(*iter)
+            );
+        }
+
+        if(generation <= 3) {
+            compare_item_lists(
+                save1->get_item_pc(),
+                save2->get_item_pc()
+            );
+        }
+
         pkmn::pokemon_party::sptr party1 = save1->get_pokemon_party();
         pkmn::pokemon_party::sptr party2 = save2->get_pokemon_party();
         EXPECT_EQ(party1->get_num_pokemon(), party2->get_num_pokemon());
         for(int i = 0; i < 6; ++i) {
-            pkmn::pokemon::sptr party_pokemon1 = party1->get_pokemon(i);
-            pkmn::pokemon::sptr party_pokemon2 = party2->get_pokemon(i);
-            EXPECT_EQ(party_pokemon1->get_species(), party_pokemon2->get_species());
+            compare_pokemon(
+                party1->get_pokemon(i),
+                party2->get_pokemon(i)
+            );
+        }
+
+        if(generation >= 2) {
+            const std::vector<std::string> box_names1 = save1->get_pokemon_pc()->get_box_names();
+            const std::vector<std::string> box_names2 = save2->get_pokemon_pc()->get_box_names();
+            ASSERT_GT(box_names1.size(), 0);
+            EXPECT_EQ(box_names1.size(), box_names2.size());
+
+            for(size_t i = 0; i < box_names1.size(); ++i) {
+                EXPECT_EQ(box_names1[i], box_names2[i]);
+            }
+        }
+
+        const pkmn::pokemon_box_list_t& boxes1 = save1->get_pokemon_pc()->as_vector();
+        const pkmn::pokemon_box_list_t& boxes2 = save2->get_pokemon_pc()->as_vector();
+        EXPECT_EQ(boxes1.size(), boxes2.size());
+        for(size_t i = 0; i < boxes1.size(); ++i) {
+            pkmn::pokemon_box::sptr box1 = boxes1[i];
+            pkmn::pokemon_box::sptr box2 = boxes2[i];
+            if(generation >= 2) {
+                EXPECT_EQ(box1->get_name(), box2->get_name());
+            }
+
+            EXPECT_EQ(box1->get_capacity(), box2->get_capacity());
+            EXPECT_EQ(box1->get_num_pokemon(), box2->get_num_pokemon());
+            for(int i = 0; i < box1->get_capacity(); ++i) {
+                compare_pokemon(
+                    box1->get_pokemon(i),
+                    box2->get_pokemon(i)
+                );
+            }
         }
     }
 }
