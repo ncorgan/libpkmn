2d15adb725a9d371f6d6e95eb16744cc47122723
Nicholas Corgan <n.corgan@gmail.com>
2017-03-17 19:16:21 -0500

    testing: finished python_game_save_test

--- testing/unit-tests/python/python_game_save_test.py
+++ testing/unit-tests/python/python_game_save_test.py
@@ -206,6 +206,74 @@ class game_save_test(unittest.TestCase):
                     else:
                         self.assertEquals(box[i].get_species(), "None")
 
+    def __get_random_pokemon(self, pokemon_list, move_list, item_list):
+        game = self.save.get_game()
+        generation = GAME_GENERATIONS[game]
+
+        species = ""
+        while True:
+            species = pokemon_list[random.randint(0, len(pokemon_list)-1)]
+            if generation > 3 or species != "Deoxys":
+                break
+
+        ret = pkmn.pokemon(species, game, "", random.randint(2, 100))
+
+        for i in range(4):
+            ret.set_move(move_list[random.randint(0, len(move_list)-1)], i)
+
+        if generation >= 2:
+            while True:
+                try:
+                    ret.set_held_item(item_list[random.randint(0, len(item_list)-1)])
+                    break
+                except ValueError:
+                    continue
+
+        return ret
+
+    def __randomize_pokemon(self, item_list):
+        game = self.save.get_game()
+        generation = GAME_GENERATIONS[game]
+        pokemon_list = pkmn.database.get_pokemon_list(generation, True)
+        move_list = pkmn.database.get_move_list(game)
+
+        party = self.save.get_pokemon_party()
+        for i in range(6):
+            party[i] = self.__get_random_pokemon(pokemon_list, move_list, item_list)
+
+        pc = self.save.get_pokemon_pc()
+        for i in range(len(pc)):
+            box = pc[i]
+            for j in range(len(box)):
+                box[j] = self.__get_random_pokemon(pokemon_list, move_list, item_list)
+
+    def __compare_item_lists(self, item_list1, item_list2):
+        self.assertEquals(item_list1.get_game(), item_list2.get_game())
+        self.assertEquals(item_list1.get_name(), item_list2.get_name())
+        self.assertEquals(len(item_list1), len(item_list2))
+        self.assertEquals(item_list1.get_num_items(), item_list2.get_num_items())
+
+        for i in range(len(item_list1)):
+            self.assertEquals(item_list1[i].item.get_name(), item_list2[i].item.get_name())
+            self.assertEquals(item_list1[i].amount, item_list2[i].amount)
+        return
+
+    def __compare_pokemon(self, pokemon1, pokemon2):
+        self.assertEquals(pokemon1.get_game(), pokemon2.get_game())
+        self.assertEquals(pokemon1.get_species(), pokemon2.get_species())
+        self.assertEquals(pokemon1.get_form(), pokemon2.get_form())
+        self.assertEquals(pokemon1.get_nickname(), pokemon2.get_nickname())
+        self.assertEquals(pokemon1.get_trainer_name(), pokemon2.get_trainer_name())
+
+        moves1 = pokemon1.get_moves()
+        moves2 = pokemon2.get_moves()
+        for i in range(4):
+            self.assertEquals(moves1[i].move.get_name(), moves2[i].move.get_name())
+            self.assertEquals(moves1[i].pp, moves2[i].pp)
+
+        if GAME_GENERATIONS[pokemon1.get_game()] >= 2:
+            self.assertEquals(pokemon1.get_held_item().get_name(), pokemon2.get_held_item().get_name())
+
     def __compare_game_saves(self):
         game = self.save.get_game()
         generation = GAME_GENERATIONS[game]
@@ -231,8 +299,25 @@ class game_save_test(unittest.TestCase):
         self.assertEquals(len(bag1), len(bag2))
         self.assertEquals(len(bag1.get_pocket_names()), len(bag2.get_pocket_names()))
         for name in bag1.get_pocket_names():
-            foo = bag1.get_pocket(name)
-            foo = bag1[name]
+            self.__compare_item_lists(bag1[name], bag2[name])
+
+        if generation <= 3:
+            self.__compare_item_lists(self.save.get_item_pc(), self.save2.get_item_pc())
+
+        party1 = self.save.get_pokemon_party()
+        party2 = self.save2.get_pokemon_party()
+        for i in range(6):
+            self.__compare_pokemon(party1[i], party2[i])
+
+        pc1 = self.save.get_pokemon_pc()
+        pc2 = self.save2.get_pokemon_pc()
+        self.assertEquals(len(pc1), len(pc2))
+        for i in range(len(pc1)):
+            box1 = pc1[i]
+            box2 = pc2[i]
+            self.assertEquals(len(box1), len(box2))
+            for j in range(len(box1)):
+                self.__compare_pokemon(box1[j], box2[j])
 
     @parameterized.expand([
         ("Red/Blue/Yellow", "Red", "red_blue", "pokemon_red.sav"),
@@ -250,7 +335,11 @@ class game_save_test(unittest.TestCase):
         self.save = pkmn.game_save(filepath)
         self.assertEquals(self.save.get_game(), expected_game)
 
+        item_list = pkmn.database.get_item_list(expected_game)
+
         self.__test_common_fields()
+        # TODO: randomize_items
+        self.__randomize_pokemon(item_list)
 
         temp_save_filepath = os.path.join(PKMN_TMP_DIR, "{0}_{1}.sav".format(expected_game, random.randint(0, 0xFFFF)))
         self.save.save_as(temp_save_filepath)
