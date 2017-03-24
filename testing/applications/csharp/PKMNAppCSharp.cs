30dfcdc0cf386c4fc8acf20a72299045dd0da8e8
Nicholas Corgan <n.corgan@gmail.com>
2017-03-23 19:45:45 -0500

    testing: added C# application made with Monodevelop

---
+++ testing/applications/csharp/PKMNAppCSharp.cs
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2017 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+
+public class PKMNAppCSharp {
+    public static void Main(string[] args) {
+        System.Console.WriteLine(PKMN.Paths.GetDatabasePath());
+    }
+}
