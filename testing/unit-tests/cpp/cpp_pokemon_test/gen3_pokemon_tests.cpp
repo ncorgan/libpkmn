6b05f1a37452223507fc89e389d17efef7b29fc2
Nicholas Corgan <n.corgan@gmail.com>
2017-05-05 18:19:11 -0500

    cpp_pokemon_test: test underlying LibPkmGC class for Gamecube test

--- testing/unit-tests/cpp/cpp_pokemon_test/gen3_pokemon_tests.cpp
+++ testing/unit-tests/cpp/cpp_pokemon_test/gen3_pokemon_tests.cpp
@@ -18,17 +18,20 @@
 #include <pksav/gba/text.h>
 #include <pksav/math/endian.h>
 
+#include "libpkmgc_includes.hpp"
+
 #include <gtest/gtest.h>
 
 #include <boost/assign.hpp>
 #include <boost/format.hpp>
+#include <boost/bimap.hpp>
 
 static const std::vector<std::string> contest_types = boost::assign::list_of
     ("Cool")("Beauty")("Cute")("Smart")("Tough")
 ;
 
 static const std::vector<std::string> contest_levels = boost::assign::list_of
-    ("")(" Super")(" Hyper")(" Master")
+    ("Super")("Hyper")("Master")
 ;
 
 static const std::vector<std::string> ribbons = boost::assign::list_of
@@ -47,13 +50,17 @@ static void check_initial_ribbons_map(
         contest_type_iter != contest_types.end();
         ++contest_type_iter)
     {
+        std::string ribbon_name = (*contest_type_iter);
+        EXPECT_EQ(1, ribbons_map.count(ribbon_name));
+        EXPECT_FALSE(ribbons_map.at(ribbon_name));
+
         for(auto contest_level_iter = contest_levels.begin();
             contest_level_iter != contest_levels.end();
             ++contest_level_iter)
         {
-            std::string ribbon_name = str(boost::format("%s%s")
-                                          % contest_type_iter->c_str()
-                                          % contest_level_iter->c_str());
+            ribbon_name = str(boost::format("%s %s")
+                              % contest_type_iter->c_str()
+                              % contest_level_iter->c_str());
             EXPECT_EQ(1, ribbons_map.count(ribbon_name));
             EXPECT_FALSE(ribbons_map.at(ribbon_name));
         }
@@ -334,6 +341,93 @@ TEST_P(gba_pokemon_test, gba_pokemon_test) {
     EXPECT_EQ(stats.at("Special Defense"), int(pksav_littleendian16(native_party_data->spdef)));
 }
 
+/*
+ * LibPkmGC stores some values in arrays, with little indication as to what each
+ * index actually corresponds to, so these enums make things easier.
+ */
+
+typedef enum {
+    LIBPKMGC_STAT_HP = 0,
+    LIBPKMGC_STAT_ATTACK,
+    LIBPKMGC_STAT_DEFENSE,
+    LIBPKMGC_STAT_SPATK,
+    LIBPKMGC_STAT_SPDEF,
+    LIBPKMGC_STAT_SPEED
+} libpkmgc_stat_t;
+
+typedef enum {
+    LIBPKMGC_CONTEST_STAT_COOL = 0,
+    LIBPKMGC_CONTEST_STAT_BEAUTY,
+    LIBPKMGC_CONTEST_STAT_CUTE,
+    LIBPKMGC_CONTEST_STAT_SMART,
+    LIBPKMGC_CONTEST_STAT_TOUGH
+} libpkmgc_contest_stat_t;
+
+typedef enum {
+    LIBPKMGC_RIBBON_CHAMPION = 0,
+    LIBPKMGC_RIBBON_WINNING,
+    LIBPKMGC_RIBBON_VICTORY,
+    LIBPKMGC_RIBBON_ARTIST,
+    LIBPKMGC_RIBBON_EFFORT,
+    LIBPKMGC_RIBBON_MARINE,
+    LIBPKMGC_RIBBON_LAND,
+    LIBPKMGC_RIBBON_SKY,
+    LIBPKMGC_RIBBON_COUNTRY,
+    LIBPKMGC_RIBBON_NATIONAL,
+    LIBPKMGC_RIBBON_EARTH,
+    LIBPKMGC_RIBBON_WORLD
+} libpkmgc_ribbon_t;
+
+typedef boost::bimap<libpkmgc_contest_stat_t, std::string> contest_stat_bimap_t;
+static const contest_stat_bimap_t CONTEST_STAT_BIMAP = boost::assign::list_of<contest_stat_bimap_t::relation>
+    (LIBPKMGC_CONTEST_STAT_COOL,   "Cool")
+    (LIBPKMGC_CONTEST_STAT_BEAUTY, "Beauty")
+    (LIBPKMGC_CONTEST_STAT_CUTE,   "Cute")
+    (LIBPKMGC_CONTEST_STAT_SMART,  "Smart")
+    (LIBPKMGC_CONTEST_STAT_TOUGH,  "Tough")
+;
+
+static const std::map<std::string, LibPkmGC::ContestAchievementLevel> CONTEST_LEVEL_MAP =
+boost::assign::map_list_of<std::string, LibPkmGC::ContestAchievementLevel>
+    ("",       LibPkmGC::NormalContestWon)
+    ("Super",  LibPkmGC::SuperContestWon)
+    ("Hyper",  LibPkmGC::HyperContestWon)
+    ("Master", LibPkmGC::MasterContestWon)
+;
+
+typedef boost::bimap<LibPkmGC::Gender, std::string> gender_bimap_t;
+static const gender_bimap_t GENDER_BIMAP = boost::assign::list_of<gender_bimap_t::relation>
+    (LibPkmGC::Male,       "Male")
+    (LibPkmGC::Female,     "Female")
+    (LibPkmGC::Genderless, "Genderless")
+;
+
+typedef boost::bimap<libpkmgc_ribbon_t, std::string> ribbon_bimap_t;
+static const ribbon_bimap_t RIBBON_BIMAP = boost::assign::list_of<ribbon_bimap_t::relation>
+    (LIBPKMGC_RIBBON_CHAMPION, "Champion")
+    (LIBPKMGC_RIBBON_WINNING,  "Winning")
+    (LIBPKMGC_RIBBON_VICTORY,  "Victory")
+    (LIBPKMGC_RIBBON_ARTIST,   "Artist")
+    (LIBPKMGC_RIBBON_EFFORT,   "Effort")
+    (LIBPKMGC_RIBBON_MARINE,   "Marine")
+    (LIBPKMGC_RIBBON_LAND,     "Land")
+    (LIBPKMGC_RIBBON_SKY,      "Sky")
+    (LIBPKMGC_RIBBON_COUNTRY,  "Country")
+    (LIBPKMGC_RIBBON_NATIONAL, "National")
+    (LIBPKMGC_RIBBON_EARTH,    "Earth")
+    (LIBPKMGC_RIBBON_WORLD,    "World")
+;
+
+typedef boost::bimap<libpkmgc_stat_t, std::string> stat_bimap_t;
+static const stat_bimap_t STAT_BIMAP = boost::assign::list_of<stat_bimap_t::relation>
+    (LIBPKMGC_STAT_HP,      "HP")
+    (LIBPKMGC_STAT_ATTACK,  "Attack")
+    (LIBPKMGC_STAT_DEFENSE, "Defense")
+    (LIBPKMGC_STAT_SPATK,   "Special Attack")
+    (LIBPKMGC_STAT_SPDEF,   "Special Defense")
+    (LIBPKMGC_STAT_SPEED,   "Speed")
+;
+
 TEST_P(gcn_pokemon_test, gcn_pokemon_test) {
     pkmn::pokemon::sptr pokemon = get_pokemon();
 
@@ -396,7 +490,84 @@ TEST_P(gcn_pokemon_test, gcn_pokemon_test) {
     EXPECT_TRUE(pokemon->is_shiny());
     EXPECT_NE(pokemon->get_personality(), personality);
 
-    // TODO: Check underlying LibPkmGC representation.
+    /*
+     * On the C++ level, check the underlying representation and make
+     * sure our abstractions match.
+     */
+    const LibPkmGC::GC::Pokemon* native = reinterpret_cast<const LibPkmGC::GC::Pokemon*>(pokemon->get_native_pc_data());
+    ASSERT_NE(nullptr, native);
+    ASSERT_EQ(nullptr, pokemon->get_native_party_data());
+
+    EXPECT_EQ(pokemon->get_database_entry().get_pokemon_index(), int(native->species));
+    EXPECT_EQ(pokemon->get_held_item().get_item_index(), int(native->heldItem));
+    EXPECT_EQ(pokemon->get_friendship(), int(native->friendship));
+    EXPECT_EQ(pkmn::database::item_entry(pokemon->get_ball(), get_game()).get_item_index(), int(native->ballCaughtWith));
+    EXPECT_EQ(pokemon->get_level_met(), int(native->levelMet));
+    // TODO: OTGender, probably bring in bimaps
+    EXPECT_STREQ(pokemon->get_trainer_name().c_str(), native->OTName->toUTF8());
+    EXPECT_STREQ(pokemon->get_nickname().c_str(), native->name->toUTF8());
+    EXPECT_EQ(pokemon->get_contest_stats().at("Feel"), int(native->contestLuster));
+
+    const std::map<std::string, bool>& markings = pokemon->get_markings();
+    EXPECT_EQ(markings.at("Circle"), native->markings.circle);
+    EXPECT_EQ(markings.at("Square"), native->markings.square);
+    EXPECT_EQ(markings.at("Triangle"), native->markings.triangle);
+    EXPECT_EQ(markings.at("Heart"), native->markings.heart);
+
+    EXPECT_EQ(pokemon->get_experience(), int(native->experience));
+    EXPECT_EQ(pokemon->get_trainer_secret_id(), native->SID);
+    EXPECT_EQ(pokemon->get_trainer_public_id(), native->TID);
+    EXPECT_EQ(pokemon->get_personality(), native->PID);
+
+    // TODO: original game
+
+    const pkmn::move_slots_t& moves = pokemon->get_moves();
+    for(size_t i = 0; i < 4; ++i) {
+        EXPECT_EQ(moves.at(i).move.get_move_id(), int(native->moves[i].move));
+        EXPECT_EQ(moves.at(i).pp, int(native->moves[i].currentPPs));
+    }
+
+    const std::map<std::string, int>& EVs = pokemon->get_EVs();
+    const std::map<std::string, int>& IVs = pokemon->get_IVs();
+    const std::map<std::string, int>& stats = pokemon->get_stats();
+    for(auto iter = STAT_BIMAP.right.begin(); iter != STAT_BIMAP.right.end(); ++iter) {
+        EXPECT_EQ(EVs.at(iter->first), int(native->EVs[iter->second]));
+        EXPECT_EQ(IVs.at(iter->first), int(native->IVs[iter->second]));
+        EXPECT_EQ(stats.at(iter->first), int(native->partyData.stats[iter->second]));
+    }
+
+    const std::map<std::string, bool>& ribbons = pokemon->get_ribbons();
+    for(auto iter = RIBBON_BIMAP.right.begin(); iter != RIBBON_BIMAP.right.end(); ++iter) {
+        EXPECT_EQ(ribbons.at(iter->first), native->specialRibbons[iter->second]);
+    }
+
+    for(auto type_iter = contest_types.begin(); type_iter != contest_types.end(); ++type_iter) {
+        if(ribbons.at(*type_iter)) {
+            EXPECT_GE(
+                native->contestAchievements[CONTEST_STAT_BIMAP.right.at(*type_iter)],
+                CONTEST_LEVEL_MAP.at("")
+            );
+        } else {
+            EXPECT_LT(
+                native->contestAchievements[CONTEST_STAT_BIMAP.right.at(*type_iter)],
+                CONTEST_LEVEL_MAP.at("")
+            );
+        }
+        for(auto level_iter = contest_levels.begin(); level_iter != contest_levels.end(); ++level_iter) {
+            std::string ribbon_name = (*type_iter) + " " + (*level_iter);
+            if(ribbons.at(ribbon_name)) {
+                EXPECT_GE(
+                    native->contestAchievements[CONTEST_STAT_BIMAP.right.at(*type_iter)],
+                    CONTEST_LEVEL_MAP.at(*level_iter)
+                );
+            } else {
+                EXPECT_LT(
+                    native->contestAchievements[CONTEST_STAT_BIMAP.right.at(*type_iter)],
+                    CONTEST_LEVEL_MAP.at(*level_iter)
+                );
+            }
+        }
+    }
 }
 
 static const std::vector<std::pair<std::string, std::string>> gba_params = {
