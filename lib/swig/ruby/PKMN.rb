28dc0ab7891f10190338e61d22e226f80d0a0511
Nicholas Corgan <n.corgan@gmail.com>
2017-08-26 07:52:00 -0500

    ruby: added GUI module

--- lib/swig/ruby/PKMN.rb
+++ lib/swig/ruby/PKMN.rb
@@ -11,6 +11,7 @@ require_relative "PKMN/PKMN"
 require_relative "PKMN/BuildInfo"
 require_relative "PKMN/Calculations"
 require_relative "PKMN/Database"
+require_relative "PKMN/GUI"
 require_relative "PKMN/Paths"
 require_relative "PKMN/STL"
 
