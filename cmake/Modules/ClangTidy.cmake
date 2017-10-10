a8ef4efa7d1c58b74022b8bd363405b41e490928
Nicholas Corgan <n.corgan@gmail.com>
2017-10-09 20:50:08 -0500

    testing: separating out static analysis, not integrated into Travis until I narrow down what flags to use

---
+++ cmake/Modules/ClangTidy.cmake
@@ -0,0 +1,20 @@
+#
+# Copyright (c) 2017 Nicholas Corgan (n.corgan@gmail.com)
+#
+# Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+# or copy at http://opensource.org/licenses/MIT)
+#
+
+SET(PKMN_USE_CLANGTIDY "${PKMN_USE_CLANGTIDY}" CACHE BOOL "Enable Clang-tidy testing")
+
+IF(PKMN_USE_CLANGTIDY)
+    if(NOT CLANG_TIDY_EXECUTABLE)
+        FIND_PROGRAM(CLANG_TIDY_EXECUTABLE NAMES clang-tidy)
+        MESSAGE(STATUS "Using clang-tidy: ${CLANG_TIDY_EXECUTABLE}")
+    ENDIF()
+
+    SET(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+    SET(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY_EXECUTABLE}")
+    SET(CLANG_TIDY_PROPERTY "${CLANG_TIDY_EXECUTABLE}" "-checks=*")
+ENDIF()
--- testing/travis-scripts/static-analysis.sh
+++ testing/travis-scripts/static-analysis.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 #
-# Copyright (c) 2016-2017 Nicholas Corgan (n.corgan@gmail.com)
+# Copyright (c) 2017 Nicholas Corgan (n.corgan@gmail.com)
 #
 # Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
 # or copy at http://opensource.org/licenses/MIT)
 #
 
 REPO_TOPLEVEL=$PWD
-mkdir -p test-env/pkmn-build
-cd test-env/pkmn-build
+mkdir -p test-env/pkmn-analysis-build
+cd test-env/pkmn-analysis-build
 [ $? -ne 0 ] && exit 1
+
+echo Running CppCheck on C++ files.
+find $REPO_TOPLEVEL/lib -name '*.[ch]pp' | xargs cppcheck --enable=performance,portability,warning --std=c++11 -I $REPO_TOPLEVEL/include --error-exitcode=1 --force --quiet
+#[ $? -ne 0 ] && exit 1
+echo Running CppCheck on C files.
+find $REPO_TOPLEVEL/lib -name '*.[ch]' | xargs cppcheck --enable=performance,portability,warning --std=c99 -I $REPO_TOPLEVEL/include --error-exitcode=1 --force --quiet
+#[ $? -ne 0 ] && exit 1
 
 if [ $PYTHON_VERSION -eq 2 ]
 then
@@ -24,48 +31,18 @@ else
     PYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.4m.so
 fi
 
+echo Building LibPKMN with Clang-tidy analysis enabled.
 cmake -DCMAKE_BUILD_TYPE=Release \
+  -DPKMN_ENABLE_CSHARP=OFF \
+  -DPKMN_ENABLE_LUA=OFF \
+  -DPKMN_ENABLE_PYTHON=OFF \
+  -DPKMN_ENABLE_RUBY=OFF \
   -DDESIRED_QT_VERSION=$DESIRED_QT_VERSION \
-  -DSWIG_EXECUTABLE=/usr/bin/swig${SWIG_VERSION}.0 \
   -DPYTHON_EXECUTABLE=/usr/bin/python${PYTHON_VERSION} \
-  -DPYTHON_INCLUDE_DIR=$PYTHON_INCLUDE_DIR \
-  -DPYTHON_LIBRARY=$PYTHON_LIBRARY \
+  -DPKMN_USE_CLANGTIDY=ON \
   $REPO_TOPLEVEL
 [ $? -ne 0 ] && exit 1
 make
-[ $? -ne 0 ] && exit 1
-ctest -E ".*GUI" --output-on-failure
-[ $? -ne 0 ] && exit 1
-# So the log isn't too verbose
-echo Installing...
-sudo make install > /dev/null 2>&1
-[ $? -ne 0 ] && exit 1
-export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
-sudo ldconfig
-[ $? -ne 0 ] && exit 1
-
-# Test external C++ project
-mkdir $REPO_TOPLEVEL/test-env/cpp-app
-[ $? -ne 0 ] && exit 1
-cd $REPO_TOPLEVEL/test-env/cpp-app
-[ $? -ne 0 ] && exit 1
-cmake $REPO_TOPLEVEL/testing/applications/cpp
-[ $? -ne 0 ] && exit 1
-make
-[ $? -ne 0 ] && exit 1
-./pkmn-cpp-app
-[ $? -ne 0 ] && exit 1
-
-# Test external C project
-mkdir $REPO_TOPLEVEL/test-env/c-app
-[ $? -ne 0 ] && exit 1
-cd $REPO_TOPLEVEL/test-env/c-app
-[ $? -ne 0 ] && exit 1
-cmake $REPO_TOPLEVEL/testing/applications/c
-[ $? -ne 0 ] && exit 1
-make
-[ $? -ne 0 ] && exit 1
-./pkmn-c-app
 [ $? -ne 0 ] && exit 1
 
 echo # So we can check the last error code
