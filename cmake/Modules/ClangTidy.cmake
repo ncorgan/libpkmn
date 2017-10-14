9ab75bb657cc79caca9b94a5d42cd2c3212b1d7c
Nicholas Corgan <n.corgan@gmail.com>
2017-10-14 18:51:03 -0500

    cmake: settled on Clang-tidy flags for now

--- cmake/Modules/ClangTidy.cmake
+++ cmake/Modules/ClangTidy.cmake
@@ -16,5 +16,11 @@ IF(PKMN_USE_CLANGTIDY)
     SET(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
     SET(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY_EXECUTABLE}")
-    SET(CLANG_TIDY_PROPERTY "${CLANG_TIDY_EXECUTABLE}" "-checks=*")
+    SET(CLANG_TIDY_PROPERTY
+        "${CLANG_TIDY_EXECUTABLE}"
+        "-checks=\
+         clang-diagnostic-*,\
+         misc-macro-parentheses,google-readability-casting,\
+         modernize-loop-convert,readability-else-after-return"
+    )
 ENDIF()
