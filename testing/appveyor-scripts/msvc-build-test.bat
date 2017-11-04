31d0cff23f70ae4eb1234b48a70ac55e2b5f6615
Nicholas Corgan <n.corgan@gmail.com>
2017-11-04 14:29:08 -0500

    appveyor: removed debug print [skip ci]

--- testing/appveyor-scripts/msvc-build-test.bat
+++ testing/appveyor-scripts/msvc-build-test.bat
@@ -3,7 +3,6 @@
 
 :: Set variables
 set BOOST_ROOT=C:/Libraries/boost_%BOOST_VERSION%_0
-dir C:\Libraries\boost_%BOOST_VERSION%_0
 set CMAKE_PREFIX_PATH=%QT_DIR%
 if "x%CMAKE_GENERATOR_NAME:2017=%"=="x%CMAKE_GENERATOR_NAME%" (
     set BOOST_LIBRARY_DIR="%BOOST_ROOT%\lib%BITNESS%-msvc-14.0"
