6fb2ae4aa3b60a47ca6d0bd94826fdc28cb55d10
Nicholas Corgan <n.corgan@gmail.com>
2017-11-04 14:59:22 -0500

    appveyor: fix Lua script for MSVC 2015

--- testing/appveyor-scripts/lua/set_compiler_env.bat
+++ testing/appveyor-scripts/lua/set_compiler_env.bat
@@ -3,17 +3,36 @@
 :: Now we declare a scope
 Setlocal EnableDelayedExpansion EnableExtensions
 
-if defined MINGW_ARCH ( goto :mingw )
+if not defined Configuration set Configuration=2015
+
+if "%Configuration%"=="MinGW" ( goto :mingw )
 
 set arch=x86
 
 if "%BITNESS%" EQU "64" ( set arch=x86_amd64 )
 
-if "x%CMAKE_GENERATOR_NAME:2017=%" == "x%CMAKE_GENERATOR_NAME" (
-    set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
-) else (
-    set SET_VS_ENV= "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat"
-)
+:: TODO: make more generic when Appveyor adds later MSVC
+set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+
+:: if "%Configuration%"=="2015" (
+:: 	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+:: )
+:: 
+:: if "%Configuration%"=="2013" (
+:: 	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
+:: )
+:: 
+:: if "%Configuration%"=="2012" (
+:: 	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
+:: )
+:: 
+:: if "%Configuration%"=="2010" (
+:: 	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+:: )
+:: 
+:: if "%Configuration%"=="2008" (
+:: 	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
+:: )
 
 :: Visual Studio detected
 endlocal & call %SET_VS_ENV% %arch%
@@ -21,4 +40,4 @@ goto :eof
 
 :: MinGW detected
 :mingw
-endlocal & set PATH=%MINGW_DIR%\bin;%PATH%
+endlocal & set PATH=c:\mingw\bin;%PATH%
