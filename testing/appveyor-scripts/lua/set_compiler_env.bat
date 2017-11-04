45dca7731b9012d8db0bb9b5816119c1467c7d13
Nicholas Corgan <n.corgan@gmail.com>
2017-11-04 13:33:56 -0500

    appveyor: fix VS2017 vcvarsall.bat location

--- testing/appveyor-scripts/lua/set_compiler_env.bat
+++ testing/appveyor-scripts/lua/set_compiler_env.bat
@@ -12,7 +12,7 @@ if "%BITNESS%" EQU "64" ( set arch=x86_amd64 )
 if "x%CMAKE_GENERATOR_NAME:2017=%" == "x%CMAKE_GENERATOR_NAME" (
     set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 ) else (
-    set SET_VS_ENV= "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
+    set SET_VS_ENV= "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat"
 )
 
 :: Visual Studio detected
