40ac7af2ccae8d170945929fdda136b3b7b7cb9b
Nicholas Corgan <n.corgan@gmail.com>
2018-01-17 20:04:33 -0600

    Added private export functions

---
+++ lib/private_exports.cpp
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2016-2017 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+
+#include "private_exports.hpp"
+
+#include "database/database_common.hpp"
+
+namespace pkmn { namespace priv {
+
+    int game_name_to_generation(
+        const std::string& game
+    )
+    {
+        return pkmn::database::game_name_to_generation(game);
+    }
+
+}}
---
+++ lib/private_exports.hpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2016-2017 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+
+#ifndef INCLUDED_PKMN_PRIVATE_FUNCTIONS_HPP
+#define INCLUDED_PKMN_PRIVATE_FUNCTIONS_HPP
+
+#include <pkmn/config.hpp>
+
+#include <string>
+
+/*
+ * Helper functions for other LibPKMN DLL's to use that require internal
+ * functionality.
+ */
+
+namespace pkmn { namespace priv {
+
+    PKMN_API int game_name_to_generation(
+        const std::string& game
+    );
+
+}}
+
+#endif /* INCLUDED_PKMN_PRIVATE_FUNCTIONS_HPP */
