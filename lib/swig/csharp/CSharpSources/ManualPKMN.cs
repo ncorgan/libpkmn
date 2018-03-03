8cda608eb059f14141fc90a2b0a646082debfcbf
Nicholas Corgan <n.corgan@gmail.com>
2018-03-02 18:50:03 -0600

    C#: manually suppress unused variable warning for database initializer

--- lib/swig/csharp/CSharpSources/ManualPKMN.cs
+++ lib/swig/csharp/CSharpSources/ManualPKMN.cs
@@ -8,6 +8,12 @@ namespace PKMN
 {
     public partial class PKMN
     {
+        /*
+         * Instantiate this class to initialize the database when the DLL is loaded
+         * rather than the first time it's actually needed. We don't actually do
+         * anything with this, so suppress the resulting warning.
+         */
+        #pragma warning disable 0414
         private static DatabaseInitializer Initializer = new DatabaseInitializer();
     }
 }
