ce72b6519880d53c5134ed9eeb0b593ba86ff7e3
Nicholas Corgan <n.corgan@gmail.com>
2018-05-27 13:35:34 -0500

    c: added header for all breeding headers

---
+++ include/pkmn-c/breeding.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2018 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+#ifndef PKMN_C_BREEDING_H
+#define PKMN_C_BREEDING_H
+
+#include <pkmn-c/breeding/child_info.h>
+#include <pkmn-c/breeding/compatibility.h>
+
+#endif /* PKMN_C_BREEDING_H */
