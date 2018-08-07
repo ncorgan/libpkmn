cd9685e07e41af869a15e5f976858aea1d2db9c7
Nicholas Corgan <n.corgan@gmail.com>
2018-08-06 21:03:59 -0500

    generate_python_enums -> generate_swig_enums

--- scripts/generate_swig_enums.py
+++ scripts/generate_swig_enums.py
@@ -18,16 +18,16 @@ CppHeaderParser.ignoreSymbols += ["class"]
 IGNORE_FILES = ["enum_to_string.hpp"]
 
 # So as to not conflict with Python keywords or internal libraries
-REPLACEMENT_FILENAMES = dict(
+PYTHON_REPLACEMENT_FILENAMES = dict(
     stat = "pokemon_stat",
     type = "pokemon_type"
 )
 
-def process_enum_header(header_path, output_dir):
+def generate_python_enum(header_path, output_dir):
     header = CppHeaderParser.CppHeader(header_path)
     for enum in header.enums:
         enum_name = enum["name"][2:]
-        filename = "{0}.py".format(REPLACEMENT_FILENAMES.get(enum_name, enum_name))
+        filename = "{0}.py".format(PYTHON_REPLACEMENT_FILENAMES.get(enum_name, enum_name))
         output_text= """#!/usr/bin/env python
 #
 # Copyright (c) 2018 Nicholas Corgan (n.corgan@gmail.com)
@@ -46,6 +46,7 @@ def process_enum_header(header_path, output_dir):
 if __name__ == "__main__":
 
     parser = OptionParser()
+    parser.add_option("--python", action="store_true", default=False)
     parser.add_option("--enum-dir", type="string", help="Enum directory")
     parser.add_option("--output-dir", type="string", help="Where in the build directory to output files")
     (options, args) = parser.parse_args()
@@ -61,4 +62,5 @@ if __name__ == "__main__":
     for root, _, files in os.walk(enum_dir):
         for filename in files:
             if (filename not in IGNORE_FILES) and filename.endswith(".hpp"):
-                process_enum_header(os.path.join(root, filename), output_dir)
+                if options.python:
+                    generate_python_enum(os.path.join(root, filename), output_dir)
